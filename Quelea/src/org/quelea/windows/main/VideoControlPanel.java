22ce779c10feb52e3cd536b7e943fa39ff30c1fe
Michael Berry <berry120@gmail.com>
2011-07-24 21:12:53 +0000

    Sort out video

--- Quelea/src/org/quelea/windows/main/VideoControlPanel.java
+++ Quelea/src/org/quelea/windows/main/VideoControlPanel.java
@@ -6,8 +6,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.HierarchyEvent;
 import java.awt.event.HierarchyListener;
 import java.awt.event.MouseAdapter;
