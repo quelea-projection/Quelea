502caa16f4db63a7e322c1b7dc0a38ddc3ec11d0
berry120 <berry120@gmail.com>
2011-07-24 20:26:19 +0000

    Email working, video ribbon control working

---
+++ Quelea/src/org/quelea/video/VideoFileFilter.java
@@ -0,0 +1,93 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2009, 2010 Caprica Software Limited.
+ */
+
+package org.quelea.video;
+
+/**
+ * File filter implementation for video files recognised by libvlc.
+ */
+public class VideoFileFilter extends ExtensionFileFilter {
+
+  /**
+   * From the vlc_interfaces.h include file.
+   */
+  private static final String[] EXTENSIONS_VIDEO = {
+    "amv",
+    "asf",
+    "avi",
+    "divx",
+    "dv",
+    "flv",
+    "gxf",
+    "iso",
+    "m1v",
+    "m2v",
+    "m2t",
+    "m2ts",
+    "m4v",
+    "mkv",
+    "mov",
+    "mp2",
+    "mp4",
+    "mpeg",
+    "mpeg1",
+    "mpeg2",
+    "mpeg4",
+    "mpg",
+    "mts",
+    "mxf",
+    "nsv",
+    "nuv",
+    "ogg",
+    "ogm",
+    "ogv",
+    "ogx",
+    "ps",
+    "rec",
+    "rm",
+    "rmvb",
+    "tod",
+    "ts",
+    "vob",
+    "vro",
+    "wmv"    
+  };
+
+  /**
+   * Single instance.
+   */
+  public static final VideoFileFilter INSTANCE = new VideoFileFilter();
+  
+  /**
+   * Create a new file filter.
+   */
+  public VideoFileFilter() {
+    super(EXTENSIONS_VIDEO);
+  }
+
+    @Override
+    public String getDescription() {
+//        StringBuilder ret = new StringBuilder("Video files: ");
+//        for(String extension : EXTENSIONS_VIDEO) {
+//            ret.append(extension).append(", ");
+//        }
+//        return ret.substring(0,ret.length()-2);
+        return "Video files";
+    }
+}
