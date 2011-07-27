2c50b1eee0b7e9a93c29166d6bad1d6e2fe1f1b0
berry120 <berry120@gmail.com>
2011-07-27 14:38:08 +0000

    Add status panel at the bottom of the frame and use it for imports

---
+++ Quelea/src/org/quelea/windows/main/StatusPanel.java
@@ -0,0 +1,60 @@
+package org.quelea.windows.main;
+
+import java.awt.Dimension;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.border.EmptyBorder;
+import org.quelea.utils.Utils;
+
+/**
+ *
+ * @author Michael
+ */
+public class StatusPanel extends JPanel {
+
+    private JProgressBar progressBar;
+    private JLabel label;
+    private JButton cancelButton;
+    private StatusPanelGroup group;
+    private int index;
+    private boolean done;
+
+    StatusPanel(StatusPanelGroup group, String labelText, int index) {
+        this.group = group;
+        this.index = index;
+        label = new JLabel(labelText);
+        progressBar = new JProgressBar();
+        cancelButton = new JButton(Utils.getImageIcon("icons/cross.png", 15, 15));
+        cancelButton.setBorder(new EmptyBorder(0, 0, 0, 0));
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        add(label);
+        add(progressBar);
+        add(cancelButton);
+    }
+
+    public void done() {
+        if (!done) {
+            group.removePanel(index);
+        }
+        done = true;
+    }
+
+    public void setLabelText(String text) {
+        label.setText(text);
+    }
+
+    public JProgressBar getProgressBar() {
+        return progressBar;
+    }
+
+    public JButton getCancelButton() {
+        return cancelButton;
+    }
+
+    public void setActive(boolean active) {
+        setVisible(active);
+    }
+}
