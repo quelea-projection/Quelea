adb829af370cb25ba70ac9fd595c92f58b67842c
berry120 <berry120@gmail.com>
2011-08-11 23:21:51 +0000

    Testing stuff

--- Quelea/src/org/quelea/video/OutOfProcessPlayer.java
+++ Quelea/src/org/quelea/video/OutOfProcessPlayer.java
@@ -22,10 +22,12 @@ import uk.co.caprica.vlcj.runtime.RuntimeUtil;
  */
 public class OutOfProcessPlayer {
 
+    private static final boolean TEST_MODE = false;
+    private EmbeddedMediaPlayer mediaPlayer;
+
     public OutOfProcessPlayer(final long canvasId) throws Exception {
 
         //Lifted pretty much out of the VLCJ code
-        EmbeddedMediaPlayer mediaPlayer;
         if (RuntimeUtil.isNix()) {
             mediaPlayer = new LinuxEmbeddedMediaPlayer(LibVlcFactory.factory().synchronise().log().create().libvlc_new(1, new String[]{"--no-video-title"}), null) {
 
@@ -65,60 +67,65 @@ public class OutOfProcessPlayer {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String inputLine;
 
-        //Process the input - I know this isn't very OO but it works for now...
-        while ((inputLine = in.readLine()) != null) {
-            if (inputLine.startsWith("open ")) {
-                inputLine = inputLine.substring("open ".length());
-                mediaPlayer.prepareMedia(inputLine);
-            }
-            else if (inputLine.equalsIgnoreCase("play")) {
-                mediaPlayer.play();
-            }
-            else if (inputLine.equalsIgnoreCase("pause")) {
-                mediaPlayer.pause();
-            }
-            else if (inputLine.equalsIgnoreCase("stop")) {
-                mediaPlayer.stop();
-            }
-            else if (inputLine.equalsIgnoreCase("playable?")) {
-                System.out.println(mediaPlayer.isPlayable());
-            }
-            else if (inputLine.startsWith("setTime ")) {
-                inputLine = inputLine.substring("setTime ".length());
-                mediaPlayer.setTime(Long.parseLong(inputLine));
-            }
-            else if (inputLine.startsWith("setMute ")) {
-                inputLine = inputLine.substring("setMute ".length());
-                mediaPlayer.mute(Boolean.parseBoolean(inputLine));
-            }
-            else if (inputLine.equalsIgnoreCase("mute?")) {
-                boolean mute = mediaPlayer.isMute();
-                System.out.println(mute);
-            }
-            else if (inputLine.equalsIgnoreCase("length?")) {
-                long length = mediaPlayer.getLength();
-                System.out.println(length);
-            }
-            else if (inputLine.equalsIgnoreCase("time?")) {
-                long time = mediaPlayer.getTime();
-                System.out.println(time);
-            }
-            else if (inputLine.equalsIgnoreCase("close")) {
-                System.exit(0);
-            }
-            else {
-                System.out.println("unknown command: ." + inputLine + ".");
+        if (!TEST_MODE) {
+            //Process the input - I know this isn't very OO but it works for now...
+            while ((inputLine = in.readLine()) != null) {
+                if (inputLine.startsWith("open ")) {
+                    inputLine = inputLine.substring("open ".length());
+                    mediaPlayer.prepareMedia(inputLine);
+                }
+                else if (inputLine.equalsIgnoreCase("play")) {
+                    mediaPlayer.play();
+                }
+                else if (inputLine.equalsIgnoreCase("pause")) {
+                    mediaPlayer.pause();
+                }
+                else if (inputLine.equalsIgnoreCase("stop")) {
+                    mediaPlayer.stop();
+                }
+                else if (inputLine.equalsIgnoreCase("playable?")) {
+                    System.out.println(mediaPlayer.isPlayable());
+                }
+                else if (inputLine.startsWith("setTime ")) {
+                    inputLine = inputLine.substring("setTime ".length());
+                    mediaPlayer.setTime(Long.parseLong(inputLine));
+                }
+                else if (inputLine.startsWith("setMute ")) {
+                    inputLine = inputLine.substring("setMute ".length());
+                    mediaPlayer.mute(Boolean.parseBoolean(inputLine));
+                }
+                else if (inputLine.equalsIgnoreCase("mute?")) {
+                    boolean mute = mediaPlayer.isMute();
+                    System.out.println(mute);
+                }
+                else if (inputLine.equalsIgnoreCase("length?")) {
+                    long length = mediaPlayer.getLength();
+                    System.out.println(length);
+                }
+                else if (inputLine.equalsIgnoreCase("time?")) {
+                    long time = mediaPlayer.getTime();
+                    System.out.println(time);
+                }
+                else if (inputLine.equalsIgnoreCase("close")) {
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("unknown command: ." + inputLine + ".");
+                }
             }
         }
     }
 
     public static void main(String[] args) {
+//        args = new String[]{"0"};
         File nativeDir = new File("lib/native");
         NativeLibrary.addSearchPath("libvlc", nativeDir.getAbsolutePath());
         NativeLibrary.addSearchPath("vlc", nativeDir.getAbsolutePath());
         try (PrintStream stream = new PrintStream(new File(QueleaProperties.getQueleaUserHome(), "ooplog.txt"))) {
             System.setErr(stream); //Important, MUST redirect err stream
-            new OutOfProcessPlayer(Integer.parseInt(args[0]));
+            OutOfProcessPlayer player = new OutOfProcessPlayer(Integer.parseInt(args[0]));
+//            player.mediaPlayer.prepareMedia("dvdsimple://E:");
+//            player.mediaPlayer.play();
         }
         catch (Exception ex) {
             ex.printStackTrace();
