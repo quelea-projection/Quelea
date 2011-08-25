a0b9cd954c9dfcfe367a342c26cd0a7b29e5d7b5
Michael Berry <berry120@gmail.com>
2011-08-25 23:12:19 +0000

    Add more attributes / fix bug

--- Quelea/src/org/quelea/windows/newsong/DetailedSongPanel.java
+++ Quelea/src/org/quelea/windows/newsong/DetailedSongPanel.java
@@ -1,11 +1,20 @@
 package org.quelea.windows.newsong;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
 import org.quelea.displayable.Song;
 import org.quelea.utils.SpringUtilities;
 
@@ -14,12 +23,14 @@ import org.quelea.utils.SpringUtilities;
  * @author Michael
  */
 public class DetailedSongPanel extends JPanel {
-    
+
     private JTextField ccli;
     private JTextField year;
     private JTextField publisher;
     private JTextField copyright;
     private JTextField tags;
+    private JTextField key;
+    private JTextArea info;
 
     /**
      * Create a new detailed song panel.
@@ -29,45 +40,126 @@ public class DetailedSongPanel extends JPanel {
         setName("Detailed information");
         JPanel formPanel = new JPanel();
         formPanel.setLayout(new SpringLayout());
-        ccli = new JTextField(10);
-        year = new JTextField(10);
+        ccli = new JTextField(new PlainDocument() {
+
+            public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+                if (str == null) {
+                    return;
+                }
+                if (str.isEmpty() || str.matches("[0-9]")) {
+                    super.insertString(offs, str, a);
+                }
+            }
+        }, "", 10);
+        year = new JTextField(new PlainDocument() {
+
+            @Override
+            public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+
+                if (str == null) {
+                    return;
+                }
+                String oldString = getText(0, getLength());
+                String newString = oldString.substring(0, offs) + str
+                        + oldString.substring(offs);
+                try {
+                    if (newString.length() <= 4) {
+                        int val = Integer.parseInt(newString + "0") / 10;
+                        if (val > 0 && val <= new GregorianCalendar().get(Calendar.YEAR) + 1) {
+                            super.insertString(offs, str, a);
+                        }
+                    }
+                }
+                catch (NumberFormatException e) {
+                    //Not a number
+                }
+            }
+        }, "", 10);
         publisher = new JTextField(10);
         copyright = new JTextField(10);
         tags = new JTextField(10);
-        
+        key = new JTextField(new PlainDocument() {
+
+            @Override
+            public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+
+                if (str == null) {
+                    return;
+                }
+                String oldString = getText(0, getLength());
+                String newString = oldString.substring(0, offs) + str
+                        + oldString.substring(offs);
+                try {
+                    if (newString.isEmpty() || newString.matches("[a-gA-G][#b]?")) {
+                        if (Character.isLowerCase(newString.charAt(0))) {
+                            str = Character.toString(Character.toUpperCase(str.substring(0, 1).charAt(0))) + str.substring(1);
+                        }
+                        super.insertString(offs, str, a);
+                    }
+                }
+                catch (NumberFormatException e) {
+                    //Not a number
+                }
+            }
+        }, "", 10);
+        info = new JTextArea(new PlainDocument() {
+
+            @Override
+            public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+
+                if (str == null) {
+                    return;
+                }
+
+                if ((getLength() + str.length()) <= 20000) {
+                    super.insertString(offs, str, a);
+                }
+            }
+        }, "", 10, 10);
+        info.setLineWrap(true);
+        info.setWrapStyleWord(true);
+
         addBlock(formPanel, "CCLI number", ccli);
         addBlock(formPanel, "Copyright", copyright);
         addBlock(formPanel, "Year", year);
         addBlock(formPanel, "Publisher", publisher);
         addBlock(formPanel, "Tags", tags);
-        
-        SpringUtilities.makeCompactGrid(formPanel, 5, 2, 6, 6, 6, 6);
-        
+        addBlock(formPanel, "Key", key);
+        JScrollPane pane = new JScrollPane(info);
+        pane.setPreferredSize(new Dimension(pane.getPreferredSize().width, 300));
+        addBlock(formPanel, "Notes", pane);
+
+        SpringUtilities.makeCompactGrid(formPanel, 7, 2, 6, 6, 6, 6);
+
         add(formPanel, BorderLayout.NORTH);
-        
+
     }
-    
-    private void addBlock(JPanel panel, String labelText, JTextField field) {
+
+    private void addBlock(JPanel panel, String labelText, Component comp) {
         JLabel label = new JLabel(labelText);
-        label.setLabelFor(field);
+        label.setLabelFor(comp);
         panel.add(label);
-        panel.add(field);
+        panel.add(comp);
     }
-    
+
     public void resetNewSong() {
         ccli.setText("");
         year.setText("");
         publisher.setText("");
         tags.setText("");
         copyright.setText("");
+        key.setText("");
+        info.setText("");
     }
-    
+
     public void resetEditSong(Song song) {
         ccli.setText(song.getCcli());
         copyright.setText(song.getCopyright());
         tags.setText(song.getTagsAsString());
         publisher.setText(song.getPublisher());
         year.setText(song.getYear());
+        key.setText(song.getKey());
+        info.setText(song.getInfo());
     }
 
     public JTextField getCcliField() {
@@ -89,7 +181,15 @@ public class DetailedSongPanel extends JPanel {
     public JTextField getYearField() {
         return year;
     }
-    
+
+    public JTextArea getInfoField() {
+        return info;
+    }
+
+    public JTextField getKeyField() {
+        return key;
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
