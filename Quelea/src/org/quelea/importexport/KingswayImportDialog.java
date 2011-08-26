5f21e72c81677b472b64afdf3b029bdb5f6e57f3
berry120 <berry120@gmail.com>
2011-08-26 00:13:49 +0000

    Better time warning

--- Quelea/src/org/quelea/importexport/KingswayImportDialog.java
+++ Quelea/src/org/quelea/importexport/KingswayImportDialog.java
@@ -15,6 +15,7 @@ public class KingswayImportDialog extends ImportDialog {
     public KingswayImportDialog(JFrame owner) {
         super(owner, new String[]{
                     "This will import the kingsway song library from online.",
+                    "This will take a long time (potentially hours!), so please be patient and leave Quelea running!"
                 }, null, new KingswayWorshipParser(), false);
     }
     
