4af5d727a7c63295b00f5704cdb7c99e21ac779c
Michael Berry <berry120@gmail.com>
2011-09-02 20:29:08 +0000

    Refactor tag entry panel into separate class

--- Quelea/src/org/quelea/tags/TagDialog.java
+++ Quelea/src/org/quelea/tags/TagDialog.java
@@ -2,6 +2,7 @@ package org.quelea.tags;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.IllegalComponentStateException;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.FocusEvent;
@@ -34,113 +35,38 @@ import org.quelea.windows.library.LibrarySongList;
  * @author Michael
  */
 public class TagDialog extends JDialog {
-    
-    private static final Logger LOGGER = LoggerUtils.getLogger();
-    private Map<String, Integer> tags;
-    private JTextField tagField;
-    private TagPanel tagPanel;
+
+    private TagEntryPanel tagEntryPanel;
     private LibrarySongList list;
-    private TagPopupWindow popup;
-    
+
     public TagDialog() {
-        super(Application.get().getMainWindow(), "Filter by tag", ModalityType.DOCUMENT_MODAL);
-        tags = new HashMap<>();
-        reloadTags();
-        popup = new TagPopupWindow();
-        popup.setTags(tags);
+        super(Application.get().getMainWindow(), "Filter by tag", ModalityType.MODELESS);
         list = new LibrarySongList();
-        tagPanel = new TagPanel();
-        tagField = new JTextField(20);
-        tagField.getDocument().addDocumentListener(new DocumentListener() {
-            
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                check();
-            }
-            
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                check();
-            }
-            
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                check();
-            }
-            
-            private void check() {
-                popup.setLocation((int) tagField.getLocationOnScreen().getX(), (int) tagField.getLocationOnScreen().getY() + tagField.getHeight());
-                popup.setString(tagField, tagPanel, list);
-            }
-        });
-        addComponentListener(new ComponentAdapter() {
-            
-            @Override
-            public void componentMoved(ComponentEvent e) {
-                if (popup.isVisible()) {
-                    popup.setLocation((int) tagField.getLocationOnScreen().getX(), (int) tagField.getLocationOnScreen().getY() + tagField.getHeight());
-                }
-            }
-        });
-        tagField.addFocusListener(new FocusListener() {
-            
-            @Override
-            public void focusGained(FocusEvent e) {
-                popup.setString(tagField, tagPanel, list);
-            }
-            
-            @Override
-            public void focusLost(FocusEvent e) {
-                popup.setVisible(false);
-            }
-        });
-        tagField.addMouseListener(new MouseAdapter() {
-            
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                popup.setString(tagField, tagPanel, list);
-            }
-        });
+        
         setLayout(new BorderLayout());
-        JPanel northPanel = new JPanel();
-        northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
-        JPanel textPanel = new JPanel();
-        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.X_AXIS));
-        textPanel.add(new JLabel("Tags:"));
-        textPanel.add(Box.createRigidArea(new Dimension(10, 0)));
-        textPanel.add(tagField);
-        northPanel.add(textPanel);
-        northPanel.add(tagPanel);
-        add(northPanel, BorderLayout.NORTH);
+        tagEntryPanel = new TagEntryPanel(list);
+        add(tagEntryPanel, BorderLayout.NORTH);
         JScrollPane scroll = new JScrollPane(list);
         scroll.setPreferredSize(new Dimension(500, 500));
         add(scroll, BorderLayout.CENTER);
         pack();
     }
     
-    public final void reloadTags() {
-        LOGGER.log(Level.INFO, "Reloading tags");
-        tags.clear();
-        for (Song song : SongDatabase.get().getSongs()) {
-            for (String tag : song.getTags()) {
-                tag = tag.trim();
-                if (tag.isEmpty()) {
-                    continue;
-                }
-                if (tags.get(tag.toLowerCase()) == null) {
-                    tags.put(tag.toLowerCase(), 1);
-                }
-                else {
-                    tags.put(tag.toLowerCase(), tags.get(tag.toLowerCase()) + 1);
-                }
-            }
-        }
+    public void reloadTags() {
+        tagEntryPanel.reloadTags();
     }
-    
+
+    @Override
+    public void setVisible(boolean visible) {
+        reloadTags();
+        setLocationRelativeTo(getParent());
+        super.setVisible(visible);
+    }
+
     public static void main(String[] args) {
         TagDialog tagDialog = new TagDialog();
         tagDialog.addWindowListener(new WindowAdapter() {
-            
+
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
