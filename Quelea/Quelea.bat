6dc9732f65787057dd3e78e94e4b40df50b912cb
Michael Berry <berry120@gmail.com>
2011-12-12 23:59:40 +0000

    Add an error if Quelea fails to start. Horrible batch file, but it's better than nothing.

--- Quelea/Quelea.bat
+++ Quelea/Quelea.bat
@@ -1,1 +1,11 @@
 java -Xms1024m -Xmx1024m -jar Quelea.jar
+IF %ERRORLEVEL% == 0 exit
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`java -Xms1024m -Xmx1024m -jar Quelea.jar`) DO (
+SET var!count!=%%F
+SET /a count=!count!+1
+)
+cscript MessageBox.vbs "It seems Quelea failed to start. A message is about to be displayed which will hopefully offer some technical debugging information that can get you some help (though it won't make much sense to you!) Please take a screenshot of it and send it to "michael@quelea.org"."
+cscript MessageBox.vbs "%var1%   ;   %var2%   ;   %var3%   ;   %var4%   ;   %var5%"
+ENDLOCAL
