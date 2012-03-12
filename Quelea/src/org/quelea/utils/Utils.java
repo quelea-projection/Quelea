5f3d2cbcf55a066171d820085295e3a27faee64c
berry120 <berry120@gmail.com>
2012-03-12 01:42:09 +0000

    - Removal of section from quick edit actually removes it from song - Temp theme is preserved when editing song in edit window - Drag images over text displayables to update their theme

--- Quelea/src/org/quelea/utils/Utils.java
+++ Quelea/src/org/quelea/utils/Utils.java
@@ -74,18 +74,21 @@ public final class Utils {
 
     /**
      * Sleep ignoring the exception.
+     *
      * @param millis milliseconds to sleep.
      */
     public static void sleep(long millis) {
         try {
             Thread.sleep(millis);
-        } catch (InterruptedException ex) {
+        }
+        catch (InterruptedException ex) {
             //Nothing
         }
     }
 
     /**
      * Determine if we're running in a 64 bit JVM.
+     *
      * @return true if it's a 64 bit JVM, false if it's 32 bit (or something
      * else.)
      */
@@ -95,14 +98,16 @@ public final class Utils {
 
     /**
      * Determine if we're running on a mac.
+     *
      * @return true if we're running on a mac, false otherwise.
      */
     public static boolean isMac() {
         return System.getProperty("os.name").toLowerCase().contains("mac");
     }
-    
+
     /**
      * Determine if we're running on Linux.
+     *
      * @return true if we're running on Linux, false otherwise.
      */
     public static boolean isLinux() {
@@ -111,31 +116,49 @@ public final class Utils {
 
     /**
      * Update a song in the background.
+     *
      * @param song the song to update.
+     * @param showError true if an error should be shown if there's a problem
+     * updating the song, false otherwise.
+     * @param silent true if we should update the song without showing a bar on
+     * the status panel.
      */
-    public static void updateSongInBackground(final Song song, final boolean showError) {
-        final StatusPanel statusPanel = Application.get().getStatusGroup().addPanel(LabelGrabber.INSTANCE.getLabel("updating.db"));
-        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+    public static void updateSongInBackground(final Song song, final boolean showError, final boolean silent) {
+        final Runnable updateRunner = new Runnable() {
 
             @Override
-            protected Void doInBackground() {
+            public void run() {
                 boolean result = SongDatabase.get().updateSong(song);
-                if (!result && showError) {
+                if(!result && showError) {
                     JOptionPane.showMessageDialog(Application.get().getMainWindow(), LabelGrabber.INSTANCE.getLabel("error.udpating.song.text"), LabelGrabber.INSTANCE.getLabel("error.text"), JOptionPane.ERROR_MESSAGE, null);
                 }
-                return null;
-            }
-
-            @Override
-            protected void done() {
-                statusPanel.done();
             }
         };
-        worker.execute();
+        if(silent) {
+            new Thread(updateRunner).start();
+        }
+        else {
+            final StatusPanel statusPanel = Application.get().getStatusGroup().addPanel(LabelGrabber.INSTANCE.getLabel("updating.db"));
+            SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+
+                @Override
+                protected Void doInBackground() {
+                    updateRunner.run();
+                    return null;
+                }
+
+                @Override
+                protected void done() {
+                    statusPanel.done();
+                }
+            };
+            worker.execute();
+        }
     }
 
     /**
      * Get the JFileChooser used for opening and saving schedules.
+     *
      * @return the JFileChooser.
      */
     public static JFileChooser getScheduleFileChooser() {
@@ -148,6 +171,7 @@ public final class Utils {
 
     /**
      * Wrap a runnable as one having a low priority.
+     *
      * @param task the runnable to wrap.
      * @return a runnable having a low priority.
      */
@@ -168,13 +192,14 @@ public final class Utils {
 
     /**
      * Determine whether translucency is supported.
+     *
      * @return true if it's supported, false otherwise.
      */
     public static boolean translucencySupported() {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] gs = ge.getScreenDevices();
-        for (GraphicsDevice device : gs) {
-            if (device.isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.TRANSLUCENT)) {
+        for(GraphicsDevice device : gs) {
+            if(device.isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.TRANSLUCENT)) {
                 return true;
             }
         }
@@ -183,24 +208,26 @@ public final class Utils {
 
     /**
      * Get a font identical to the one given apart from in size.
+     *
      * @param font the original font.
      * @param size the size of the new font.
      * @return the resized font.
      */
     public static Font getDifferentSizeFont(Font font, float size) {
         Map<TextAttribute, Object> attributes = new HashMap<>();
-        for (Entry<TextAttribute, ?> entry : font.getAttributes().entrySet()) {
+        for(Entry<TextAttribute, ?> entry : font.getAttributes().entrySet()) {
             attributes.put(entry.getKey(), entry.getValue());
         }
-        if (attributes.get(TextAttribute.SIZE) != null) {
+        if(attributes.get(TextAttribute.SIZE) != null) {
             attributes.put(TextAttribute.SIZE, size);
         }
         return new Font(attributes);
     }
 
     /**
-     * Calculates the largest size of the given font for which the given string 
+     * Calculates the largest size of the given font for which the given string
      * will fit into the given size.
+     *
      * @param g the graphics to use in the current context.
      * @param font the original font to base the returned font on.
      * @param string the string to fit.
@@ -213,15 +240,16 @@ public final class Utils {
         int maxSize = 288;
         int curSize = font.getSize();
 
-        while (maxSize - minSize > 2) {
+        while(maxSize - minSize > 2) {
             FontMetrics fm = g.getFontMetrics(new Font(font.getName(), font.getStyle(), curSize));
             int fontWidth = fm.stringWidth(string);
             int fontHeight = fm.getLeading() + fm.getMaxAscent() + fm.getMaxDescent();
 
-            if ((fontWidth > width) || (fontHeight > height)) {
+            if((fontWidth > width) || (fontHeight > height)) {
                 maxSize = curSize;
                 curSize = (maxSize + minSize) / 2;
-            } else {
+            }
+            else {
                 minSize = curSize;
                 curSize = (minSize + maxSize) / 2;
             }
@@ -233,6 +261,7 @@ public final class Utils {
     /**
      * Get the difference between two colours, from 0 to 100 where 100 is most
      * difference and 0 is least different.
+     *
      * @param a the first colour
      * @param b the second colour
      * @return the difference between the colours.
@@ -244,6 +273,7 @@ public final class Utils {
 
     /**
      * Remove all HTML tags from a string.
+     *
      * @param str the string to remove the tags from.
      * @return the string with the tags removed.
      */
@@ -253,7 +283,8 @@ public final class Utils {
 
     /**
      * Determine whether the given frame is completely on the given screen.
-     * @param frame      the frame to check.
+     *
+     * @param frame the frame to check.
      * @param monitorNum the monitor number to check.
      * @return true if the frame is totally on the screen, false otherwise.
      */
@@ -265,7 +296,8 @@ public final class Utils {
 
     /**
      * Centre the given frame on the given monitor.
-     * @param frame      the frame to centre.
+     *
+     * @param frame the frame to centre.
      * @param monitorNum the monitor number to centre the frame on.
      */
     public static void centreOnMonitor(final JFrame frame, int monitorNum) {
@@ -281,24 +313,26 @@ public final class Utils {
                 frame.setLocation(centreX - frame.getWidth() / 2, centreY - frame.getHeight() / 2);
             }
         };
-        if (SwingUtilities.isEventDispatchThread()) {
+        if(SwingUtilities.isEventDispatchThread()) {
             locationSetter.run();
-        } else {
+        }
+        else {
             SwingUtilities.invokeLater(locationSetter);
         }
     }
 
     /**
      * Remove duplicates in a list whilst maintaining the order.
-     * @param <T>  the type of the list.
+     *
+     * @param <T> the type of the list.
      * @param list the list to remove duplicates.
      */
     public static <T> void removeDuplicateWithOrder(List<T> list) {
         Set<T> set = new HashSet<>();
         List<T> newList = new ArrayList<>();
-        for (Iterator<T> iter = list.iterator(); iter.hasNext();) {
+        for(Iterator<T> iter = list.iterator(); iter.hasNext();) {
             T element = iter.next();
-            if (set.add(element)) {
+            if(set.add(element)) {
                 newList.add(element);
             }
         }
@@ -308,22 +342,23 @@ public final class Utils {
 
     /**
      * Copy a file from one place to another.
+     *
      * @param sourceFile the source file
      * @param destFile the destination file
      * @throws IOException if something goes wrong.
      */
     public static void copyFile(File sourceFile, File destFile) throws IOException {
-        if (sourceFile.isDirectory()) {
-            if (sourceFile.getName().equals(".svn")) {
+        if(sourceFile.isDirectory()) {
+            if(sourceFile.getName().equals(".svn")) {
                 return;
             }
             Files.copy(sourceFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            for (File file : sourceFile.listFiles()) {
+            for(File file : sourceFile.listFiles()) {
                 copyFile(file, new File(destFile, file.getName()));
             }
             return;
         }
-        if (!destFile.exists()) {
+        if(!destFile.exists()) {
             destFile.createNewFile();
         }
 
@@ -333,11 +368,12 @@ public final class Utils {
             source = new FileInputStream(sourceFile).getChannel();
             destination = new FileOutputStream(destFile).getChannel();
             destination.transferFrom(source, 0, source.size());
-        } finally {
-            if (source != null) {
+        }
+        finally {
+            if(source != null) {
                 source.close();
             }
-            if (destination != null) {
+            if(destination != null) {
                 destination.close();
             }
         }
@@ -345,11 +381,12 @@ public final class Utils {
 
     /**
      * Capitalise the first letter of a string.
+     *
      * @param line the input string.
      * @return the the string with the first letter capitalised.
      */
     public static String capitaliseFirst(String line) {
-        if (line.isEmpty()) {
+        if(line.isEmpty()) {
             return line;
         }
         StringBuilder ret = new StringBuilder(line);
@@ -358,15 +395,17 @@ public final class Utils {
     }
 
     /**
-     * Get an abbreviation from a name based on the first letter of each word of the name.
+     * Get an abbreviation from a name based on the first letter of each word of
+     * the name.
+     *
      * @param name the name to use for the abbreviation.
      * @return the abbreviation.
      */
     public static String getAbbreviation(String name) {
         StringBuilder ret = new StringBuilder();
         String[] parts = name.split(" ");
-        for (String str : parts) {
-            if (!str.isEmpty()) {
+        for(String str : parts) {
+            if(!str.isEmpty()) {
                 ret.append(Character.toUpperCase(str.charAt(0)));
             }
         }
@@ -375,6 +414,7 @@ public final class Utils {
 
     /**
      * Escape the XML special characters.
+     *
      * @param s the string to escape.
      * @return the escaped string.
      */
@@ -383,22 +423,24 @@ public final class Utils {
     }
 
     /**
-     * Get the textual content from a file as a string, returning the given error string if a problem occurs retrieving
-     * the content.
-     * @param fileName  the filename to get the text from.
+     * Get the textual content from a file as a string, returning the given
+     * error string if a problem occurs retrieving the content.
+     *
+     * @param fileName the filename to get the text from.
      * @param errorText the error string to return if things go wrong.
-     * @return hopefully the text content of the file, or the errorText string if we can't get the text content for some
-     *         reason.
+     * @return hopefully the text content of the file, or the errorText string
+     * if we can't get the text content for some reason.
      */
     public static synchronized String getTextFromFile(String fileName, String errorText) {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             StringBuilder content = new StringBuilder();
             String line;
-            while ((line = reader.readLine()) != null) {
+            while((line = reader.readLine()) != null) {
                 content.append(line).append('\n');
             }
             return content.toString();
-        } catch (IOException ex) {
+        }
+        catch (IOException ex) {
             LOGGER.log(Level.WARNING, "Couldn't get the contents of " + fileName, ex);
             return errorText;
         }
@@ -406,8 +448,10 @@ public final class Utils {
 
     /**
      * Get an image icon from the location of a specified file.
+     *
      * @param location the location of the image to use.
-     * @return the icon formed from the image, or null if an IOException occured.
+     * @return the icon formed from the image, or null if an IOException
+     * occured.
      */
     public static ImageIcon getImageIcon(String location) {
         return getImageIcon(location, -1, -1);
@@ -415,14 +459,16 @@ public final class Utils {
 
     /**
      * Get an image icon from the location of a specified file.
+     *
      * @param location the location of the image to use.
      * @param width the width of the given image icon.
      * @param height the height of the given image icon.
-     * @return the icon formed from the image, or null if an IOException occured.
+     * @return the icon formed from the image, or null if an IOException
+     * occured.
      */
     public static ImageIcon getImageIcon(String location, int width, int height) {
         Image image = getImage(location, width, height);
-        if (image == null) {
+        if(image == null) {
             return null;
         }
         return new ImageIcon(image);
@@ -430,8 +476,10 @@ public final class Utils {
 
     /**
      * Get an image from the location of a specified file.
+     *
      * @param location the location of the image to use.
-     * @return the icon formed from the image, or null if an IOException occured.
+     * @return the icon formed from the image, or null if an IOException
+     * occured.
      */
     public static BufferedImage getImage(String location) {
         return getImage(location, -1, -1);
@@ -439,20 +487,24 @@ public final class Utils {
 
     /**
      * Get an image from the location of a specified file.
+     *
      * @param location the location of the image to use.
      * @param width the width of the returned image.
-     * @param height the height of the returned image. 
-     * @return the icon formed from the image, or null if an IOException occured.
+     * @param height the height of the returned image.
+     * @return the icon formed from the image, or null if an IOException
+     * occured.
      */
     public static BufferedImage getImage(String location, int width, int height) {
         try {
             BufferedImage image = ImageIO.read(new File(location));
-            if (width > 0 && height > 0) {
+            if(width > 0 && height > 0) {
                 return resizeImage(image, width, height);
-            } else {
+            }
+            else {
                 return image;
             }
-        } catch (IOException ex) {
+        }
+        catch (IOException ex) {
             LOGGER.log(Level.WARNING, "Couldn't get image: " + location, ex);
             return null;
         }
@@ -460,26 +512,29 @@ public final class Utils {
 
     /**
      * Resize a given image to the given width and height.
+     *
      * @param image the image to resize.
      * @param width the width of the new image.
      * @param height the height of the new image.
      * @return the resized image.
      */
     public static BufferedImage resizeImage(BufferedImage image, int width, int height) {
-        if (width > 0 && height > 0 && (image.getWidth() != width || image.getHeight() != height)) {
+        if(width > 0 && height > 0 && (image.getWidth() != width || image.getHeight() != height)) {
             BufferedImage bdest = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g = bdest.createGraphics();
             g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
             g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
             g.drawImage(image, 0, 0, width, height, null);
             return bdest;
-        } else {
+        }
+        else {
             return image;
         }
     }
 
     /**
      * Convert the given icon to an image.
+     *
      * @param icon the icon to convert.
      * @return the converted icon.
      */
@@ -491,13 +546,15 @@ public final class Utils {
 
     /**
      * Determine whether a file is an image file.
+     *
      * @param file the file to check.
      * @return true if the file is an image, false otherwise.
      */
     public static boolean fileIsImage(File file) {
-        if (file.isDirectory() && !file.isHidden()) {
+        if(file.isDirectory() && !file.isHidden()) {
             return true;
-        } else {
+        }
+        else {
             String suffix = file.getName().split("\\.")[file.getName().split("\\.").length - 1].toLowerCase().trim();
             return (suffix.equals("png")
                     || suffix.equals("bmp")
@@ -510,16 +567,17 @@ public final class Utils {
 
     /**
      * Get the names of all the fonts available on the current system.
+     *
      * @return the names of all the fonts available.
      */
     public static String[] getAllFonts() {
         Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
         Set<String> names = new HashSet<>();
-        for (int i = 0; i < fonts.length; i++) {
+        for(int i = 0; i < fonts.length; i++) {
             names.add(fonts[i].getFamily());
         }
         List<String> namesList = new ArrayList<>(names.size());
-        for (String name : names) {
+        for(String name : names) {
             namesList.add(name);
         }
         Collections.sort(namesList);
@@ -528,8 +586,9 @@ public final class Utils {
 
     /**
      * Get an image filled with the specified colour.
-     * @param color  the colour of the image.
-     * @param width  the width of the image.
+     *
+     * @param color the colour of the image.
+     * @param width the width of the image.
      * @param height the height of the image.
      * @return the image.
      */
@@ -543,6 +602,7 @@ public final class Utils {
 
     /**
      * Parse a colour string to a colour.
+     *
      * @param colour the colour string.
      * @return the colour.
      */
