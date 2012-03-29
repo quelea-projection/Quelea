78878eec4b8f1efaff2048e9c22dc3218be57da5
Michael Berry <berry120@gmail.com>
2012-03-29 09:15:27 +0000

    Update bible search

--- Quelea/src/org/quelea/bible/BibleSearchDialog.java
+++ Quelea/src/org/quelea/bible/BibleSearchDialog.java
@@ -18,6 +18,7 @@
 package org.quelea.bible;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
@@ -26,12 +27,18 @@ import java.awt.event.KeyEvent;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ListCellRenderer;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import org.quelea.Application;
+import org.quelea.displayable.TextSection;
 import org.quelea.languages.LabelGrabber;
 import org.quelea.utils.Utils;
 
@@ -43,7 +50,7 @@ import org.quelea.utils.Utils;
 public class BibleSearchDialog extends JDialog {
 
     private JTextField searchField;
-    private JList<String> searchResults;
+    private JList<BibleChapter> searchResults;
 
     /**
      * Create a new bible searcher dialog.
@@ -53,24 +60,35 @@ public class BibleSearchDialog extends JDialog {
         setLayout(new BorderLayout());
         setTitle(LabelGrabber.INSTANCE.getLabel("bible.search.title"));
         setIconImage(Utils.getImage("icons/search.png"));
-        searchField = new JTextField(20);
+        searchField = new JTextField(30);
         JPanel northPanel = new JPanel();
         northPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         northPanel.add(searchField);
         add(northPanel, BorderLayout.NORTH);
-        searchResults = new JList<>(new DefaultListModel<String>());
+        searchResults = new JList<>(new DefaultListModel<BibleChapter>());
+        searchResults.setCellRenderer(new SearchPreviewRenderer());
         JPanel centrePanel = new JPanel();
         centrePanel.setLayout(new BoxLayout(centrePanel, BoxLayout.Y_AXIS));
         centrePanel.add(new JScrollPane(searchResults));
         add(centrePanel, BorderLayout.CENTER);
-        searchField.addKeyListener(new KeyAdapter() {
+        searchField.getDocument().addDocumentListener(new DocumentListener() {
 
             @Override
-            public void keyTyped(KeyEvent ke) {
+            public void insertUpdate(DocumentEvent de) {
+                update();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent de) {
+                update();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent de) {
                 update();
             }
         });
-        pack();
+        setSize(600,400);
         reset();
     }
 
@@ -78,7 +96,7 @@ public class BibleSearchDialog extends JDialog {
      * Reset this dialog.
      */
     public final void reset() {
-        DefaultListModel<String> model = (DefaultListModel<String>) searchResults.getModel();
+        DefaultListModel<BibleChapter> model = (DefaultListModel<BibleChapter>) searchResults.getModel();
         model.clear();
         searchField.setText(LabelGrabber.INSTANCE.getLabel("initial.search.text"));
         searchField.addFocusListener(new FocusAdapter() {
@@ -109,11 +127,11 @@ public class BibleSearchDialog extends JDialog {
 
                 @Override
                 public void run() {
-                    DefaultListModel<String> model = (DefaultListModel<String>) searchResults.getModel();
+                    DefaultListModel<BibleChapter> model = (DefaultListModel<BibleChapter>) searchResults.getModel();
                     model.clear();
                     if(!text.trim().isEmpty()) {
                         for(BibleChapter chapter : results) {
-                            model.addElement(chapter.getText());
+                            model.addElement(chapter);
                         }
                     }
                 }
@@ -144,5 +162,57 @@ public class BibleSearchDialog extends JDialog {
         BibleSearchDialog dialog = new BibleSearchDialog();
 //        dialog.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
         dialog.setVisible(true);
+    }
+
+    /**
+     * Renderer for displaying a preview of the part of the bible chapter
+     * containing the search text.
+     */
+    private class SearchPreviewRenderer extends JLabel implements ListCellRenderer<BibleChapter> {
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public Component getListCellRendererComponent(JList<? extends BibleChapter> list, BibleChapter value, int index, boolean isSelected, boolean cellHasFocus) {
+            String introText = "<b>" + value.getBook().getBookName() + " " + value.getNum() + " (" + value.getBook().getBible().getName() + ")" + ": </b>";
+            String searchText = searchField.getText().trim().toLowerCase();
+            String passageText = value.getText().trim();
+            int pos = passageText.toLowerCase().indexOf(searchText);
+            int startIndex = pos - 10;
+            while(startIndex >= 0 && !Character.isWhitespace(value.getText().charAt(startIndex))) {
+                startIndex++;
+            }
+            if(startIndex < 0) {
+                startIndex = 0;
+            }
+            int endIndex = pos + 10 + searchText.length();
+            while(endIndex < passageText.length() && !Character.isWhitespace(value.getText().charAt(endIndex))) {
+                endIndex++;
+            }
+            if(endIndex > passageText.length()) {
+                endIndex = passageText.length();
+            }
+            String subStr = "..." + passageText.substring(startIndex, endIndex).trim() + "...";
+            setBorder(new EmptyBorder(5, 5, 5, 5));
+            StringBuilder labelHTML = new StringBuilder();
+            labelHTML.append("<html>");
+            labelHTML.append(introText);
+            labelHTML.append(subStr);
+            labelHTML.append("</html>");
+            setText(labelHTML.toString());
+            if(isSelected) {
+                setBackground(list.getSelectionBackground());
+                setForeground(list.getSelectionForeground());
+            }
+            else {
+                setBackground(list.getBackground());
+                setForeground(list.getForeground());
+            }
+            setEnabled(list.isEnabled());
+            setFont(list.getFont());
+            setOpaque(true);
+            return this;
+        }
     }
 }
