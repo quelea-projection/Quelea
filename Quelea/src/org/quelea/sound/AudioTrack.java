499faa041be004a9e98e4ee8bb8078fcad5b8a8b
Michael Berry <berry120@gmail.com>
2012-05-22 00:05:21 +0000

    Deal with corrupt audio files properly

--- Quelea/src/org/quelea/sound/AudioTrack.java
+++ Quelea/src/org/quelea/sound/AudioTrack.java
@@ -20,10 +20,13 @@ package org.quelea.sound;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import net.sourceforge.jaad.spi.javasound.AACAudioFileReader;
+import org.quelea.utils.LoggerUtils;
 
 /**
  * An audio track that can be played by the audio player.
@@ -32,6 +35,7 @@ import net.sourceforge.jaad.spi.javasound.AACAudioFileReader;
  */
 public class AudioTrack {
 
+    private static final Logger LOGGER = LoggerUtils.getLogger();
     private String path;
 
     /**
@@ -41,6 +45,19 @@ public class AudioTrack {
      */
     public AudioTrack(String path) {
         this.path = path;
+    }
+
+    public boolean checkOK() {
+        AudioInputStream stream = getAudioInputStream();
+        if(stream != null) {
+            try {
+                stream.close();
+            }
+            catch (IOException ex) {
+                LOGGER.log(Level.WARNING, "Couldn't close audio input stream", ex);
+            }
+        }
+        return stream != null;
     }
 
     /**
@@ -58,8 +75,8 @@ public class AudioTrack {
      * @return the track's audio input stream.
      */
     public AudioInputStream getAudioInputStream() {
+        File file = new File(path);
         try {
-            File file = new File(path);
             AudioInputStream in;
             String parsedPath = path.toLowerCase().trim();
             //Workaround, AAC doesn't have SPI - and if we give it SPI capability, it hogs everything then fails if it can't play it.
@@ -79,6 +96,7 @@ public class AudioTrack {
             return in;
         }
         catch (UnsupportedAudioFileException | IOException ex) {
+            LOGGER.log(Level.WARNING, "Couldn't get audio input stream: " + file.getAbsolutePath(), ex);
             return null;
         }
     }
