78e099d93afd8eacd0c902090d07c67ea46cb947
berry120 <berry120@gmail.com>
2012-06-29 01:03:41 +0000

    Update launch.sh to work with Ubuntu 12.04

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -4,7 +4,7 @@ echo Default Java failed, locating Java 7
 /usr/bin/update-alternatives --query java | while read line
 do
   if [ ! -z "$(echo $line | awk '/Alternative:/')" ]; then
-    if [ ! -z "$(echo $line | awk '/java-7-openjdk/')" ]; then
+    if [ ! -z "$(echo $line | awk '/java-7-openjdk')" ]; then
       line=$(echo $line | cut -d ":" -f2)
       $line -Xms1024m -Xmx1024m -jar Quelea.jar
       exit
