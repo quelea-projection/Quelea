4a27fc92e43393d973122f99e4d6b7633a7a7bab
tomaszpio <tomaszpio@gmail.com>
2012-11-11 23:37:31 +0000

    packages refactoring. Moved some packages in to data package

--- Quelea/src/org/quelea/data/tags/TagPopupWindow.java
+++ Quelea/src/org/quelea/data/tags/TagPopupWindow.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.quelea.tags;
+package org.quelea.data.tags;
 
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
