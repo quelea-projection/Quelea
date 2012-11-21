bb9b635a8373c1afc091d88cffa00b49c62b5db8
user <user@user-HP>
2012-11-22 00:07:47 +0100

    packages refactoring

--- Quelea/src/org/quelea/windows/main/schedule/ScheduleSongPopupMenu.java
+++ Quelea/src/org/quelea/windows/main/schedule/ScheduleSongPopupMenu.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.quelea.windows.main;
+package org.quelea.windows.main.schedule;
 
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
