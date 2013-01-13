2ae141e767d2816d20c39418677afc29a4d80e04
Michael Berry <berry120@gmail.com>
2013-01-13 22:57:20 +0000

    - Remove add / edit / remove song buttons from schedule menu - not really a great place for them to start with, and they're only duplicating functionality elsewhere.  - Fix the email button

--- Quelea/src/org/quelea/windows/main/menus/ScheduleMenu.java
+++ Quelea/src/org/quelea/windows/main/menus/ScheduleMenu.java
@@ -21,6 +21,8 @@ package org.quelea.windows.main.menus;
 import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ListChangeListener.Change;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -59,13 +61,13 @@ import org.quelea.windows.main.widgets.ButtonChecker;
 public class ScheduleMenu extends Menu {
 
     private static final Logger LOGGER = LoggerUtils.getLogger();
-    private MenuItem addSongItem;
-    private MenuItem editSongItem;
-    private MenuItem removeSongItem;
+//    private MenuItem addSongItem;
+//    private MenuItem editSongItem;
+//    private MenuItem removeSongItem;
     private MenuItem addPowerpointItem;
     private MenuItem addVideoItem;
     private MenuItem addAudioItem;
-    private MenuItem addDVDItem;
+//    private MenuItem addDVDItem;
     private MenuItem manageNoticesItem;
     private MenuItem shareScheduleItem;
 
@@ -74,38 +76,38 @@ public class ScheduleMenu extends Menu {
      */
     public ScheduleMenu() {
         super(LabelGrabber.INSTANCE.getLabel("schedule.menu"));
-        addSongItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("add.song.button"), new ImageView(new Image("file:icons/newsong.png", 16, 16, false, true)));
-        addSongItem.setOnAction(new AddSongActionHandler());
-        addSongItem.setDisable(true);
-        getItems().add(addSongItem);
+//        addSongItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("add.song.button"), new ImageView(new Image("file:icons/newsong.png", 16, 16, false, true)));
+//        addSongItem.setOnAction(new AddSongActionHandler());
+//        addSongItem.setDisable(true);
+//        getItems().add(addSongItem);
 
-        editSongItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("edit.song.button"), new ImageView(new Image("file:icons/edit.png", 16, 16, false, true)));
-        editSongItem.setOnAction(new EditSongScheduleActionHandler());
-        editSongItem.setDisable(true);
-        getItems().add(editSongItem);
+//        editSongItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("edit.song.button"), new ImageView(new Image("file:icons/edit.png", 16, 16, false, true)));
+//        editSongItem.setOnAction(new EditSongScheduleActionHandler());
+//        editSongItem.setDisable(true);
+//        getItems().add(editSongItem);
 
-        removeSongItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("remove.item.button"), new ImageView(new Image("file:icons/remove 2.png", 16, 16, false, true)));
-        removeSongItem.setOnAction(new RemoveScheduleItemActionHandler());
-        removeSongItem.setDisable(true);
-        getItems().add(removeSongItem);
+//        removeSongItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("remove.item.button"), new ImageView(new Image("file:icons/remove 2.png", 16, 16, false, true)));
+//        removeSongItem.setOnAction(new RemoveScheduleItemActionHandler());
+//        removeSongItem.setDisable(true);
+//        getItems().add(removeSongItem);
 
         final MainPanel mainPanel = QueleaApp.get().getMainWindow().getMainPanel();
         final ScheduleList scheduleList = mainPanel.getSchedulePanel().getScheduleList();
-        scheduleList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Displayable>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Displayable> ov, Displayable t, Displayable t1) {
-                ButtonChecker.INSTANCE.checkEditRemoveButtons(editSongItem, removeSongItem);
-            }
-        });
-        final LibrarySongList songList = mainPanel.getLibraryPanel().getLibrarySongPanel().getSongList();
-        songList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<SongDisplayable>() {
-
-            @Override
-            public void changed(ObservableValue<? extends SongDisplayable> ov, SongDisplayable t, SongDisplayable t1) {
-                ButtonChecker.INSTANCE.checkAddButton(addSongItem);
-            }
-        });
+//        scheduleList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Displayable>() {
+//
+//            @Override
+//            public void changed(ObservableValue<? extends Displayable> ov, Displayable t, Displayable t1) {
+//                ButtonChecker.INSTANCE.checkEditRemoveButtons(editSongItem, removeSongItem);
+//            }
+//        });
+//        final LibrarySongList songList = mainPanel.getLibraryPanel().getLibrarySongPanel().getSongList();
+//        songList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<SongDisplayable>() {
+//
+//            @Override
+//            public void changed(ObservableValue<? extends SongDisplayable> ov, SongDisplayable t, SongDisplayable t1) {
+//                ButtonChecker.INSTANCE.checkAddButton(addSongItem);
+//            }
+//        });
 
         getItems().add(new SeparatorMenuItem());
 
@@ -135,10 +137,10 @@ public class ScheduleMenu extends Menu {
 
         shareScheduleItem = new MenuItem(LabelGrabber.INSTANCE.getLabel("email.button"), new ImageView(new Image("file:icons/email.png", 16, 16, false, true)));
         shareScheduleItem.setDisable(true);
-        scheduleList.itemsProperty().addListener(new ChangeListener<ObservableList<Displayable>>() {
+        scheduleList.getItems().addListener(new ListChangeListener<Displayable>() {
 
             @Override
-            public void changed(ObservableValue<? extends ObservableList<Displayable>> ov, ObservableList<Displayable> t, ObservableList<Displayable> t1) {
+            public void onChanged(Change<? extends Displayable> change) {
                 Schedule schedule = scheduleList.getSchedule();
                 if(schedule == null || !schedule.iterator().hasNext()) {
                     shareScheduleItem.setDisable(true);
