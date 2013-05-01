2e3c1f90b856ca3f22d8f377c8157d388a8e1ce6
berry120 <berry120@localhost>
2013-05-01 22:19:04 +0100

    Fix a few things with build script / changelog

--- Quelea/src/org/quelea/services/utils/VersionChecker.java
+++ Quelea/src/org/quelea/services/utils/VersionChecker.java
@@ -20,6 +20,7 @@ package org.quelea.services.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.logging.Level;
