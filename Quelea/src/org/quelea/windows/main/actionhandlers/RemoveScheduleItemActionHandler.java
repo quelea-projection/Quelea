f4c691ca61e0273ea1d6319c7e7028e89796f7d5
Michael Berry <berry120@gmail.com>
2013-07-03 18:10:45 +0100

    Use delete to remove song on schedule list

--- Quelea/src/org/quelea/windows/main/actionhandlers/RemoveScheduleItemActionHandler.java
+++ Quelea/src/org/quelea/windows/main/actionhandlers/RemoveScheduleItemActionHandler.java
@@ -32,7 +32,7 @@ public class RemoveScheduleItemActionHandler implements EventHandler<ActionEvent
     /**
      * Remove the currently selected song from the schedule.
      * <p/>
-     * @param e the action event.
+     * @param e the action event (ignored.)
      */
     @Override
     public void handle(ActionEvent t) {
