7db180e7d50fe9fccc507329f4b0cf9b0e9509e6
Michael Berry <berry120@gmail.com>
2013-07-09 18:09:37 +0100

    - Can now search songs using all UTF-8 characters - All songs now appear in alphabetical order on initial load (some were initially out of place) - Platform thread is not blocked anymore when updating database

--- Quelea/src/org/quelea/data/db/SongManager.java
+++ Quelea/src/org/quelea/data/db/SongManager.java
@@ -20,10 +20,11 @@ package org.quelea.data.db;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.quelea.data.ThemeDTO;
@@ -38,7 +39,7 @@ import org.quelea.services.utils.LoggerUtils;
 
 /**
  * Manage songs persistent operations.
- *
+ * <p/>
  * @author Michael
  */
 public final class SongManager {
@@ -62,7 +63,7 @@ public final class SongManager {
 
     /**
      * Get the singleton instance of this class.
-     *
+     * <p/>
      * @return the singleton instance of this class.
      */
     public static SongManager get() {
@@ -71,7 +72,7 @@ public final class SongManager {
 
     /**
      * Get the underlying search index used by this database.
-     *
+     * <p/>
      * @return the search index.
      */
     public SearchIndex<SongDisplayable> getIndex() {
@@ -80,7 +81,7 @@ public final class SongManager {
 
     /**
      * Determine if an error occurred initialising the database.
-     *
+     * <p/>
      * @return true if an error occurred, false if all is ok.
      */
     public boolean errorOccurred() {
@@ -89,7 +90,7 @@ public final class SongManager {
 
     /**
      * Register a database listener with this database.
-     *
+     * <p/>
      * @param listener the listener.
      */
     public void registerDatabaseListener(DatabaseListener listener) {
@@ -99,27 +100,29 @@ public final class SongManager {
     /**
      * Fire off the database listeners.
      */
-    public void fireUpdate() {
-        for (DatabaseListener listener : listeners) {
+    public synchronized void fireUpdate() {
+        for(DatabaseListener listener : listeners) {
             listener.databaseChanged();
         }
     }
 
     /**
      * Get all the songs in the database.
-     *
+     * <p/>
      * @return an array of all the songs in the database.
      */
-    public SongDisplayable[] getSongs() {
-
-        final List<SongDisplayable> songs = new ArrayList<>();
+    public synchronized SongDisplayable[] getSongs() {
+        if(Platform.isFxApplicationThread()) {
+            LOGGER.log(Level.WARNING, "getSongs() should not be called on platform thread!", new RuntimeException("Debug exception"));
+        }
+        final Set<SongDisplayable> songs = new TreeSet<>();
         HibernateUtil.execute(new HibernateUtil.SessionCallback() {
             @Override
             public void execute(Session session) {
-                for (Song song : new SongDao(session).getSongs()) {
+                for(Song song : new SongDao(session).getSongs()) {
                     final String[] tags = new String[song.getTags().size()];
 
-                    for (int i = 0; i < song.getTags().size(); i++) {
+                    for(int i = 0; i < song.getTags().size(); i++) {
                         tags[i] = song.getTags().get(i);
                     }
                     final SongDisplayable songDisplayable = new SongDisplayable.Builder(song.getTitle(),
@@ -135,7 +138,7 @@ public final class SongManager {
                             .capo(song.getCapo())
                             .id(song.getId()).get();
                     final Theme theme = song.getTheme();
-                    for (TextSection section : songDisplayable.getSections()) {
+                    for(TextSection section : songDisplayable.getSections()) {
                         section.setTheme(ThemeDTO.getDTO(theme));
                     }
                     song.setTheme(theme);
@@ -144,9 +147,9 @@ public final class SongManager {
             }
         });
 
-        if (!addedToIndex) {
+        if(!addedToIndex) {
             addedToIndex = true;
-            LOGGER.log(Level.INFO, "Adding " + songs.size() + " songs to index");
+            LOGGER.log(Level.INFO, "Adding {0} songs to index", songs.size());
             index.addAll(songs);
         }
         return songs.toArray(new SongDisplayable[songs.size()]);
@@ -154,14 +157,16 @@ public final class SongManager {
 
     /**
      * Add a song to the database.
-     *
+     * <p/>
      * @param song the song to add.
      * @param fireUpdate true if the update should be fired to listeners when
      * adding this song, false otherwise.
      * @return true if the operation succeeded, false otherwise.
      */
-    public boolean addSong(final SongDisplayable song, boolean fireUpdate) {
-        if(song.getSections().length == 0) return false;
+    public synchronized boolean addSong(final SongDisplayable song, final boolean fireUpdate) {
+        if(song.getSections().length == 0) {
+            return false;
+        }
         final boolean nullTheme = song.getSections()[0].getTheme() == null;
         final boolean nullTags = song.getTags() == null;
         HibernateUtil.execute(new HibernateUtil.SessionCallback() {
@@ -182,8 +187,10 @@ public final class SongManager {
                 session.save(newSong);
             }
         });
-
-        if (fireUpdate) {
+        if(addedToIndex) {
+            index.add(song);
+        }
+        if(fireUpdate) {
             fireUpdate();
         }
         return true;
@@ -191,11 +198,11 @@ public final class SongManager {
 
     /**
      * Update a song in the database.
-     *
+     * <p/>
      * @param song the song to update.
      * @return true if the operation succeeded, false otherwise.
      */
-    public boolean updateSong(final SongDisplayable song) {
+    public synchronized boolean updateSong(final SongDisplayable song) {
         HibernateUtil.execute(new HibernateUtil.SessionCallback() {
             @Override
             public void execute(Session session) {
@@ -217,24 +224,32 @@ public final class SongManager {
                     updatedSong.setTitle(song.getTitle());
                     updatedSong.setTheme(nullTheme ? ThemeDTO.DEFAULT_THEME.getTheme() : new Theme(song.getSections()[0].getTheme().getTheme()));
                     session.update(updatedSong);
-                } catch (ObjectNotFoundException e) {
+                    new Thread() {
+                        public void run() {
+                            if(addedToIndex) {
+                                index.remove(song);
+                                index.add(song);
+                            }
+                        }
+                    }.start();
+                }
+                catch(ObjectNotFoundException e) {
                     LOGGER.log(Level.INFO, "Updating song that doesn't exist, adding instead");
                     addSong(song, true);
                 }
-
             }
         });
-        fireUpdate();
+
         return true;
     }
 
     /**
      * Remove a song from the database.
-     *
+     * <p/>
      * @param song the song to remove.
      * @return true if the operation succeeded, false otherwise.
      */
-    public boolean removeSong(final SongDisplayable song) {
+    public synchronized boolean removeSong(final SongDisplayable song) {
         HibernateUtil.execute(new HibernateUtil.SessionCallback() {
             @Override
             public void execute(Session session) {
