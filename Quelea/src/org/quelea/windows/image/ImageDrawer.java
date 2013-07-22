0cc3742e069b9c9b6be75b827f7716674319e055
berry120 <berry120@localhost>
2013-07-22 22:08:22 +0100

    Display images with grey border in live / preview panels, and preserve aspect ratio

--- Quelea/src/org/quelea/windows/image/ImageDrawer.java
+++ Quelea/src/org/quelea/windows/image/ImageDrawer.java
@@ -2,32 +2,43 @@ package org.quelea.windows.image;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import org.quelea.data.displayable.Displayable;
 import org.quelea.data.displayable.ImageDisplayable;
 import org.quelea.windows.main.DisplayableDrawer;
+import org.quelea.windows.main.QueleaApp;
 
 /**
- *
+ * Responsible for drawing an image onto the DisplayCanvas.
+ * <p/>
  * @author tomaszpio@gmail.com
+ * @author berry120@gmail.com
  */
 public class ImageDrawer extends DisplayableDrawer {
 
-    private ImageView imageView = null;
-    private Image image = null;
+    private ImageView imageView;
+    private Image image;
 
     @Override
     public void draw(Displayable displayable) {
-        image = ((ImageDisplayable)displayable).getImage();
+        clear();
+        image = ((ImageDisplayable) displayable).getImage();
         imageView = getCanvas().getNewImageView();
-        imageView.setFitHeight(getCanvas().getHeight());
         imageView.setFitWidth(getCanvas().getWidth());
+        imageView.setPreserveRatio(true);
         imageView.setImage(image);
-        getCanvas().getChildren().add(imageView);
+        StackPane imageBox = new StackPane();
+        imageBox.getChildren().add(imageView);
+        if(getCanvas() != QueleaApp.get().getProjectionWindow().getCanvas()
+                && getCanvas() != QueleaApp.get().getStageWindow().getCanvas()) {
+            imageBox.setStyle("-fx-background-color:#dddddd;");
+        }
+        getCanvas().getChildren().add(imageBox);
     }
 
     @Override
     public void clear() {
-        if (getCanvas().getChildren() != null) {
+        if(getCanvas().getChildren() != null) {
             getCanvas().getChildren().clear();
         }
     }
