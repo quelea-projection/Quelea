41eea1d8e1da83400789eafd7210b5d9bb818b17
Michael Berry <berry120@gmail.com>
2013-09-07 12:15:22 +0100

    More video interface improvements

--- Quelea/src/org/quelea/windows/multimedia/MultimediaPanel.java
+++ Quelea/src/org/quelea/windows/multimedia/MultimediaPanel.java
@@ -17,59 +17,72 @@
  */
 package org.quelea.windows.multimedia;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import org.quelea.data.displayable.Displayable;
+import org.quelea.data.displayable.MultimediaDisplayable;
 import org.quelea.windows.main.AbstractPanel;
 import org.quelea.windows.main.DisplayCanvas;
-import org.quelea.windows.main.DisplayCanvas.Priority;
 import org.quelea.windows.main.DisplayableDrawer;
 
 /**
  * A panel used in the live / preview panels for playing audio.
- *
+ * <p/>
  * @author tomaszpio@gmail.com
  */
 public class MultimediaPanel extends AbstractPanel {
 
-    private final DisplayCanvas previewCanvas;
     private MultimediaDrawer drawer;
     private MultimediaControls controlPanel;
+    private Text previewText;
 
     /**
      * Create a new image panel.
      */
     public MultimediaPanel() {
         this.controlPanel = new MultimediaControls();
+        controlPanel.setDisableControls(true);
         drawer = new MultimediaDrawer(controlPanel);
-        previewCanvas = new MultimediaPreviewCanvas(false, false, new DisplayCanvas.CanvasUpdater() {
+        ImageView img = new ImageView(new Image("file:icons/vid preview.png"));
+        BorderPane.setMargin(controlPanel, new Insets(30));
+        setTop(controlPanel);
+        VBox centerBit = new VBox(5);
+        centerBit.setAlignment(Pos.CENTER);
+        previewText = new Text();
+        previewText.setFont(Font.font("Verdana", 20));
+        previewText.setFill(Color.WHITE);
+        BorderPane.setMargin(centerBit, new Insets(10));
+        centerBit.getChildren().add(previewText);
+        centerBit.getChildren().add(img);
+        setCenter(centerBit);
+        setMinWidth(50);
+        setStyle("-fx-background-color:grey;");
+        DisplayCanvas dummyCanvas = new DisplayCanvas(false, false, false, new DisplayCanvas.CanvasUpdater() {
             @Override
             public void updateCallback() {
                 updateCanvas();
             }
-        }, Priority.LOW);
-        registerDisplayCanvas(previewCanvas);
-        setCenter(controlPanel);
+        }, DisplayCanvas.Priority.LOW);
+        registerDisplayCanvas(dummyCanvas);
     }
 
     @Override
-    public void registerDisplayCanvas(DisplayCanvas canvas) {
-        super.registerDisplayCanvas(canvas);
-    }
-    
-    @Override
     public void updateCanvas() {
+        MultimediaDisplayable displayable = (MultimediaDisplayable) getCurrentDisplayable();
+        previewText.setText(displayable.getFile().getName());
         for(DisplayCanvas canvas : getCanvases()) {
             drawer.setCanvas(canvas);
             drawer.setPlayVideo(canvas.getPlayVideo());
-            canvas.setCurrentDisplayable(getCurrentDisplayable());
-            drawer.draw(getCurrentDisplayable());
+            canvas.setCurrentDisplayable(displayable);
+            drawer.draw(displayable);
         }
-    }
-
-    /**
-     * Clear the panel and all canvases associated with it.
-     */
-    @Override
-    public void clear() {
-        super.clear();
     }
 
     @Override
