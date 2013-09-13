8749d58df20c4e4f64e5d2518d1f4aad46115409
Michael Berry <berry120@gmail.com>
2013-09-13 01:29:03 +0100

    Set correct window owner for options dialog

--- Quelea/src/org/quelea/windows/options/OptionsDialog.java
+++ Quelea/src/org/quelea/windows/options/OptionsDialog.java
@@ -58,6 +58,7 @@ public class OptionsDialog extends Stage {
         setTitle(LabelGrabber.INSTANCE.getLabel("options.title"));
         initModality(Modality.APPLICATION_MODAL);
         initStyle(StageStyle.UTILITY);
+        initOwner(QueleaApp.get().getMainWindow());
         setResizable(false);
         
         getIcons().add(new Image("file:icons/options.png", 16, 16, false, true));
