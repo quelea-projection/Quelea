3639bb18b67f66b919a6e7fd16616d7d910aa4ca
Ben Goodwin <beng92@users.noreply.github.com>
2014-01-15 00:24:29 +0000

    Quick re-implementation of logo

---
+++ Quelea/src/org/quelea/windows/main/LivePanel.java
@@ -0,0 +1,266 @@
+/* 
+ * This file is part of Quelea, free projection software for churches.
+ * 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.quelea.windows.main;
+
+import java.io.File;
+import java.util.HashSet;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.stage.FileChooser;
+import javafx.stage.Screen;
+import org.quelea.data.displayable.Displayable;
+import org.quelea.services.languages.LabelGrabber;
+import org.quelea.services.utils.FileFilters;
+import org.quelea.services.utils.QueleaProperties;
+import org.quelea.services.utils.Utils;
+import org.quelea.windows.multimedia.VLCWindow;
+
+/**
+ * The panel displaying the live lyrics selection - changes made on this panel
+ * are reflected on the live projection.
+ * <p/>
+ * @author Michael
+ */
+public class LivePanel extends LivePreviewPanel {
+
+    private final ToggleButton logo;
+    private final ToggleButton black;
+    private final ToggleButton clear;
+    private final ToggleButton hide;
+
+    /**
+     * Create a new live lyrics panel.
+     */
+    public LivePanel() {
+        getPresentationPanel().setLive();
+        ToolBar header = new ToolBar();
+        Label headerLabel = new Label(LabelGrabber.INSTANCE.getLabel("live.heading"));
+        headerLabel.setStyle("-fx-font-weight: bold;");
+        header.getItems().add(headerLabel);
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        header.getItems().add(spacer);
+        logo = new ToggleButton("", new ImageView(new Image("file:icons/logo16.png")));
+        Utils.setToolbarButtonStyle(logo);
+        logo.setTooltip(new Tooltip(LabelGrabber.INSTANCE.getLabel("logo.screen.tooltip")));
+        logo.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if(t.getButton().equals(MouseButton.SECONDARY)) {
+                    FileChooser chooser = new FileChooser();
+                    chooser.getExtensionFilters().add(FileFilters.IMAGES);
+                    chooser.setInitialDirectory(QueleaProperties.get().getImageDir().getAbsoluteFile());
+                    File file = chooser.showOpenDialog(QueleaApp.get().getMainWindow());
+                    if(file != null) {
+                        updateLogo();
+                    }
+                }
+            }
+        });
+        logo.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent t) {
+                HashSet<DisplayCanvas> canvases = new HashSet<>();
+                canvases.addAll(getCanvases());
+                for(DisplayCanvas canvas : canvases) {
+                    if(!canvas.isStageView()) {
+                        canvas.setLogoDisplaying(logo.isSelected());
+                    }
+                }
+            }
+        });
+        header.getItems().add(logo);
+        black = new ToggleButton("", new ImageView(new Image("file:icons/black.png")));
+        Utils.setToolbarButtonStyle(black);
+        black.setTooltip(new Tooltip(LabelGrabber.INSTANCE.getLabel("black.screen.tooltip") + " (F5)"));
+        black.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent t) {
+                HashSet<DisplayCanvas> canvases = new HashSet<>();
+                canvases.addAll(getCanvases());
+                for(DisplayCanvas canvas : canvases) {
+                    canvas.setBlacked(black.isSelected());
+                }
+            }
+        });
+        header.getItems().add(black);
+        clear = new ToggleButton("", new ImageView(new Image("file:icons/clear.png", 16, 16, false, true)));
+        Utils.setToolbarButtonStyle(clear);
+        clear.setTooltip(new Tooltip(LabelGrabber.INSTANCE.getLabel("clear.text.tooltip") + " (F6)"));
+        clear.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent t) {
+                HashSet<DisplayCanvas> canvases = new HashSet<>();
+                canvases.addAll(getCanvases());
+                for(DisplayCanvas canvas : canvases) {
+                    canvas.setCleared(clear.isSelected());
+                }
+            }
+        });
+        header.getItems().add(clear);
+        hide = new ToggleButton("", new ImageView(new Image("file:icons/cross.png")));
+        Utils.setToolbarButtonStyle(hide);
+        hide.setTooltip(new Tooltip(LabelGrabber.INSTANCE.getLabel("hide.display.output.tooltip") + " (F7)"));
+        hide.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent t) {
+                int projectorScreen = QueleaProperties.get().getProjectorScreen();
+                int stageScreen = QueleaProperties.get().getStageScreen();
+                final ObservableList<Screen> monitors = Screen.getScreens();
+
+                DisplayStage appWindow = QueleaApp.get().getProjectionWindow();
+                DisplayStage stageWindow = QueleaApp.get().getStageWindow();
+
+                final boolean lyricsHidden;
+                if(!QueleaProperties.get().isProjectorModeCoords() && (projectorScreen >= monitors.size() || projectorScreen < 0)) {
+                    lyricsHidden = true;
+                }
+                else {
+                    lyricsHidden = false;
+                }
+
+                final boolean stageHidden;
+                if(!QueleaProperties.get().isStageModeCoords() && (stageScreen >= monitors.size() || stageScreen < 0)) {
+                    stageHidden = true;
+                }
+                else {
+                    stageHidden = false;
+                }
+
+                if(!lyricsHidden) {
+                    if(hide.isSelected()) {
+                        appWindow.hide();
+                    }
+                    else {
+                        appWindow.show();
+                    }
+                }
+                if(!stageHidden) {
+                    if(hide.isSelected()) {
+                        stageWindow.hide();
+                    }
+                    else {
+                        stageWindow.show();
+                    }
+                }
+                VLCWindow.INSTANCE.setHideButton(hide.isSelected());
+            }
+        });
+        header.getItems().add(hide);
+        setTop(header);
+        setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent t) {
+                if(t.getCharacter().equals(" ")) {
+                    QueleaApp.get().getMainWindow().getMainPanel().getPreviewPanel().goLive();
+                }
+            }
+        });
+    }
+
+    /**
+     * Set the displayable to be shown on this live panel.
+     * <p/>
+     * @param d the displayable to show.
+     * @param index the index to use for the displayable, if relevant.
+     */
+    @Override
+    public void setDisplayable(Displayable d, int index) {
+        super.setDisplayable(d, index);
+        if(d == null) {
+            clear.setSelected(false);
+            clear.setDisable(true);
+        }
+        else {
+            clear.setDisable(!d.supportClear());
+            if(!d.supportClear()) {
+                clear.setSelected(false);
+            }
+        }
+        HashSet<DisplayCanvas> canvases = new HashSet<>();
+        canvases.addAll(getCanvases());
+        for(DisplayCanvas canvas : canvases) {
+            canvas.setBlacked(black.isSelected());
+            canvas.setCleared(clear.isSelected());
+        }
+    }
+
+    /**
+     * Toggle the "black" button.
+     */
+    public void toggleBlack() {
+        if(!black.isDisable()) {
+            black.fire();
+        }
+    }
+
+    /**
+     * Toggle the "clear" button.
+     */
+    public void toggleClear() {
+        if(!clear.isDisable()) {
+            clear.fire();
+        }
+    }
+
+    /**
+     * Toggle the "hide" button.
+     */
+    public void toggleHide() {
+        if(!hide.isDisable()) {
+            hide.fire();
+        }
+    }
+
+    /**
+     * Get the hide button.
+     * <p>
+     * @return the hide button.
+     */
+    public ToggleButton getHide() {
+        return hide;
+    }
+    
+    private void updateLogo() {
+        HashSet<DisplayCanvas> canvases = new HashSet<>();
+        canvases.addAll(getCanvases());
+        for (DisplayCanvas canvas : canvases) {
+            canvas.updateLogo();
+        }
+    }
+    
+    public void updateCanvases() {
+        HashSet<DisplayCanvas> canvases = new HashSet<>();
+        canvases.addAll(getCanvases());
+        for(DisplayCanvas canvas : canvases) {
+            canvas.update();
+        }
+    }
+}
---
+++ Quelea/src/org/quelea/windows/multimedia/MultimediaControls.java
@@ -0,0 +1,193 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.quelea.windows.multimedia;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.control.Slider;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * The multimedia controls containing a play / pause button, stop button, and a
+ * position slider on a gradient background.
+ * <p/>
+ * @author Michael
+ */
+public class MultimediaControls extends StackPane {
+
+    private static final int SLIDER_UPDATE_RATE = 100;
+    private static final Image PLAY_IMAGE = new Image("file:icons/play.png");
+    private static final Image PAUSE_IMAGE = new Image("file:icons/pause.png");
+    private static final Image STOP_IMAGE = new Image("file:icons/stop.png");
+    private static final Image PLAY_IMAGE_DISABLE = new Image("file:icons/playdisable.png");
+    private static final Image PAUSE_IMAGE_DISABLE = new Image("file:icons/pausedisable.png");
+    private static final Image STOP_IMAGE_DISABLE = new Image("file:icons/stopdisable.png");
+    private boolean playpause;
+    private ImageView playButton;
+    private ImageView stopButton;
+    private Slider posSlider;
+    private boolean disableControls;
+
+    public MultimediaControls() {
+        Rectangle rect = new Rectangle(230, 100);
+        Stop[] stops = new Stop[]{new Stop(0, Color.BLACK), new Stop(1, Color.GREY)};
+        LinearGradient lg1 = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, stops);
+        rect.setOpacity(0.8);
+        rect.setFill(lg1);
+        rect.setArcHeight(20);
+        rect.setArcWidth(20);
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(5.0);
+        ds.setOffsetX(5.0);
+        ds.setColor(Color.GRAY);
+        ds.setInput(new Reflection(5, 0.4, 0.3, 0));
+        rect.setEffect(ds);
+        getChildren().add(rect);
+
+        playButton = new ImageView(PLAY_IMAGE);
+        setButtonParams(playButton);
+        playButton.setTranslateX(-30);
+        getChildren().add(playButton);
+        playButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if(!disableControls) {
+                    playpause = !playpause;
+                    if(playpause) {
+                        playButton.setImage(PAUSE_IMAGE);
+                        VLCWindow.INSTANCE.setRepeat(false);
+                        VLCWindow.INSTANCE.play();
+                        posSlider.setDisable(false);
+                    }
+                    else {
+                        playButton.setImage(PLAY_IMAGE);
+                        VLCWindow.INSTANCE.pause();
+                    }
+                }
+            }
+        });
+
+        stopButton = new ImageView(STOP_IMAGE);
+        stopButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if(!disableControls) {
+                    reset();
+                }
+            }
+        });
+        setButtonParams(stopButton);
+        stopButton.setTranslateX(30);
+        getChildren().add(stopButton);
+
+        posSlider = new Slider(0, 1, 0);
+        posSlider.setDisable(true);
+        posSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number t, Number t1) {
+                if(!disableControls && (VLCWindow.INSTANCE.isPlaying() || VLCWindow.INSTANCE.isPaused()) && posSlider.isValueChanging()) {
+                    VLCWindow.INSTANCE.setProgressPercent(posSlider.getValue());
+                }
+            }
+        });
+        posSlider.setTranslateY(30);
+        posSlider.setPrefWidth(rect.getWidth() - 20);
+        posSlider.setMaxWidth(rect.getWidth() - 20);
+        getChildren().add(posSlider);
+
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                if(!disableControls && VLCWindow.INSTANCE.isPlaying() && !posSlider.isValueChanging()) {
+                    posSlider.setValue(VLCWindow.INSTANCE.getProgressPercent());
+                }
+            }
+        }, 0, SLIDER_UPDATE_RATE, TimeUnit.MILLISECONDS);
+        VLCWindow.INSTANCE.setOnFinished(new Runnable() {
+            @Override
+            public void run() {
+                if(getScene() != null && !disableControls) {
+                    reset();
+                }
+            }
+        });
+    }
+
+    public void loadMultimedia(String path) {
+        reset();
+        VLCWindow.INSTANCE.load(path);
+    }
+
+    public void setDisableControls(boolean disable) {
+        this.disableControls = disable;
+        if(disable) {
+            if(!playpause) {
+                playButton.setImage(PLAY_IMAGE_DISABLE);
+            }
+            else {
+                playButton.setImage(PAUSE_IMAGE_DISABLE);
+            }
+            stopButton.setImage(STOP_IMAGE_DISABLE);
+        }
+        else {
+            if(!playpause) {
+                playButton.setImage(PLAY_IMAGE);
+            }
+            else {
+                playButton.setImage(PAUSE_IMAGE);
+            }
+            stopButton.setImage(STOP_IMAGE);
+        }
+    }
+
+    public void reset() {
+        VLCWindow.INSTANCE.stop();
+        if(disableControls) {
+            playButton.setImage(PLAY_IMAGE_DISABLE);
+        }
+        else {
+            playButton.setImage(PLAY_IMAGE);
+        }
+        playpause = false;
+        posSlider.setValue(0);
+        posSlider.setDisable(true);
+    }
+
+    private void setButtonParams(final ImageView button) {
+        button.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if(!disableControls) {
+                    button.setEffect(new Glow(0.5));
+                }
+            }
+        });
+        button.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                button.setEffect(null);
+            }
+        });
+        button.setFitWidth(50);
+        button.setPreserveRatio(true);
+        button.setTranslateY(-10);
+    }
+}
