2b7ade608443baa75a78e73f92ee07999e2af08d
Michael Berry <berry120@gmail.com>
2014-01-16 23:36:57 +0000

    Fix logo and black not working on video display

---
+++ Quelea/src/org/quelea/windows/main/DisplayStage.java
@@ -0,0 +1,137 @@
+/*
+ * This file is part of Quelea, free projection software for churches.
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.quelea.windows.main;
+
+import java.util.logging.Logger;
+import javafx.scene.Cursor;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Bounds;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.quelea.services.languages.LabelGrabber;
+import org.quelea.services.utils.LoggerUtils;
+import org.quelea.services.utils.Utils;
+import org.quelea.windows.main.DisplayCanvas.Priority;
+import org.quelea.windows.main.widgets.Clock;
+import org.quelea.windows.multimedia.VLCWindow;
+
+/**
+ * The full screen window used for displaying the projection.
+ * <p/>
+ * @author Michael
+ */
+public class DisplayStage extends Stage {
+
+    private static final Logger LOGGER = LoggerUtils.getLogger();
+    private static final Cursor BLANK_CURSOR;
+    private final DisplayCanvas canvas;
+
+    /**
+     * Initialise cursor hiding.
+     */
+    static {
+        BLANK_CURSOR = javafx.scene.Cursor.NONE;
+    }
+
+    /**
+     * Create a new display window positioned to fill the given rectangle.
+     * <p/>
+     * @param area the area in which the window should be drawn.
+     */
+    public DisplayStage(Bounds area, boolean stageView) {
+        final boolean playVideo = !stageView;
+        initStyle(StageStyle.TRANSPARENT);
+        Utils.addIconsToStage(this);
+        setTitle(LabelGrabber.INSTANCE.getLabel("projection.window.title"));
+        setArea(area);
+        StackPane scenePane = new StackPane();
+        canvas = new DisplayCanvas(true, stageView, playVideo, null, stageView ? Priority.HIGH : Priority.MID);
+        canvas.setType(stageView ? DisplayCanvas.Type.STAGE : DisplayCanvas.Type.FULLSCREEN);
+        canvas.setCursor(BLANK_CURSOR);
+        scenePane.getChildren().add(canvas);
+        if(stageView) {
+            Clock clock = new Clock();
+            StackPane.setAlignment(clock, Pos.BOTTOM_RIGHT);
+            scenePane.getChildren().add(clock);
+            clock.toFront();
+        }
+        Scene scene = new Scene(scenePane);
+        scene.setFill(null);
+        setScene(scene);
+        if(playVideo) {
+            addVLCListeners();
+        }
+    }
+
+    private void addVLCListeners() {
+        widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                VLCWindow.INSTANCE.refreshPosition();
+            }
+        });
+        heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                VLCWindow.INSTANCE.refreshPosition();
+            }
+        });
+        xProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                VLCWindow.INSTANCE.refreshPosition();
+            }
+        });
+        yProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                VLCWindow.INSTANCE.refreshPosition();
+            }
+        });
+    }
+
+    /**
+     * Set the area of the display window.
+     * <p/>
+     * @param area the area of the window.
+     */
+    public final void setArea(final Bounds area) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                setWidth(area.getMaxX() - area.getMinX());
+                setHeight(area.getMaxY() - area.getMinY());
+                setX(area.getMinX());
+                setY(area.getMinY());
+            }
+        });
+    }
+
+    /**
+     * Get the canvas object that underlines this display window.
+     * <p/>
+     * @return the lyric canvas backing this window.
+     */
+    public DisplayCanvas getCanvas() {
+        return canvas;
+    }
+}
