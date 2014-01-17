e5d4d8459a1520aba07df4787d481f23fe591b3b
bengoodwin13 <bengoodwin13@localhost>
2014-01-17 12:49:36 +0000

    Change default behaviour of StageView with no LogoImage implementation

--- Quelea/src/org/quelea/windows/main/widgets/LogoImage.java
+++ Quelea/src/org/quelea/windows/main/widgets/LogoImage.java
@@ -20,24 +20,29 @@ package org.quelea.windows.main.widgets;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import org.quelea.services.utils.QueleaProperties;
+import org.quelea.services.utils.Utils;
+import org.quelea.windows.main.QueleaApp;
 
 /**
  * The logo image - on a separate stack pane with its background colour set so
  * we don't see any content behind the panel when its aspect ratio doesn't line
  * up exactly with the display canvas size!
  * <p>
+ *
  * @author Michael
  */
 public class LogoImage extends StackPane {
 
     private final ImageView logoImage;
+    private boolean stageView;
 
     /**
      * Create a new logo image
      */
-    public LogoImage() {
-        setStyle("-fx-background-color:#000000;");
+    public LogoImage(boolean stageView) {
+        this.stageView = stageView;
         logoImage = new ImageView();
+        setStyle("-fx-background-color:#000000;");
         refresh();
         getChildren().add(logoImage);
     }
@@ -46,10 +51,14 @@ public class LogoImage extends StackPane {
      * Update this logo image with the correct one from the properties file
      */
     public final void refresh() {
-        logoImage.setImage(QueleaProperties.get().getLogoImage());
-        logoImage.setPreserveRatio(true);
+        if (stageView) {
+            logoImage.setImage(Utils.getImageFromColour(QueleaProperties.get().getStageBackgroundColor()));
+        }
+        else {
+            logoImage.setImage(QueleaProperties.get().getLogoImage());
+            logoImage.setPreserveRatio(true);
+        }
         logoImage.fitWidthProperty().bind(widthProperty());
         logoImage.fitHeightProperty().bind(heightProperty());
     }
-
 }
