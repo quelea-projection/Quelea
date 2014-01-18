202356379e3cd643c906300efad2e1d1f96b76b3
Michael Berry <berry120@gmail.com>
2014-01-18 01:34:00 +0000

    Make some other displayable types serializable

--- Quelea/src/org/quelea/data/displayable/VideoDisplayable.java
+++ Quelea/src/org/quelea/data/displayable/VideoDisplayable.java
@@ -18,6 +18,7 @@
 package org.quelea.data.displayable;
 
 import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -31,7 +32,7 @@ import org.w3c.dom.Node;
  *
  * @author Michael
  */
-public class VideoDisplayable implements MultimediaDisplayable {
+public class VideoDisplayable implements MultimediaDisplayable, Serializable {
 
     private final File file;
     /**
