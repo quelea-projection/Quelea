80a7b95b834753fcb4035389d2a64c1754288dc1
Michael Berry <berry120@gmail.com>
2014-05-17 15:47:17 +0100

    Start support for translations. Displaying the translations is still left to do and will require a fair bit of work, the database / UI for entering and altering translations is now completed.

---
+++ Quelea/src/org/quelea/windows/lyrics/TranslateDialog.java
@@ -0,0 +1,178 @@
+/*
+ * This file is part of Quelea, free projection software for churches.
+ * 
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.quelea.windows.lyrics;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.quelea.data.displayable.SongDisplayable;
+import org.quelea.services.languages.LabelGrabber;
+import org.quelea.services.utils.LoggerUtils;
+import org.quelea.services.utils.Utils;
+
+/**
+ * The translation dialog that manages all the translations - translations can
+ * be added or removed from here.
+ *
+ * @author Michael
+ */
+public class TranslateDialog extends Stage {
+
+    private static final Logger LOGGER = LoggerUtils.getLogger();
+    private final SplitPane splitPane;
+    private final TabPane tabPane;
+    private final TextArea defaultLyricsArea;
+    private final Button addTranslationButton;
+    private final Button closeButton;
+
+    /**
+     * Create the translation dialog.
+     */
+    public TranslateDialog() {
+        initModality(Modality.APPLICATION_MODAL);
+        Utils.addIconsToStage(this);
+        setTitle(LabelGrabber.INSTANCE.getLabel("translate.dialog.title"));
+        splitPane = new SplitPane();
+        defaultLyricsArea = new TextArea();
+        defaultLyricsArea.setEditable(false);
+        StackPane translationPane = new StackPane();
+        tabPane = new TabPane();
+        translationPane.getChildren().add(tabPane);
+        addTranslationButton = new Button("", new ImageView(new Image("file:icons/newstar.png", 16, 16, false, true)));
+        Utils.setToolbarButtonStyle(addTranslationButton);
+        addTranslationButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent t) {
+                String name = NewTranslationDialog.getTranslationName();
+                if (name != null) {
+                    TranslateTab tab = new TranslateTab(name, "");
+                    tabPane.getTabs().add(tab);
+                }
+            }
+        });
+        addTranslationButton.setTooltip(new Tooltip(LabelGrabber.INSTANCE.getLabel("add.translation.button")));
+        StackPane.setAlignment(addTranslationButton, Pos.TOP_RIGHT);
+        translationPane.getChildren().add(addTranslationButton);
+        StackPane.setMargin(addTranslationButton, new Insets(5));
+        BorderPane leftArea = new BorderPane();
+        StackPane topRegion = new StackPane();
+        Text defaultTranslationLabel = new Text(LabelGrabber.INSTANCE.getLabel("default.translation.label"));
+        defaultTranslationLabel.setFill(Color.WHITE);
+        StackPane.setAlignment(defaultTranslationLabel, Pos.CENTER_LEFT);
+        StackPane.setMargin(defaultTranslationLabel, new Insets(5));
+        topRegion.getChildren().add(defaultTranslationLabel);
+        topRegion.setPrefHeight(31);
+        topRegion.setStyle("-fx-background-color:rgb(166,166,166);"); //Match the tabpane header background.
+        topRegion.setMaxWidth(Double.MAX_VALUE);
+        leftArea.setTop(topRegion);
+        leftArea.setCenter(defaultLyricsArea);
+        splitPane.getItems().add(leftArea);
+        splitPane.getItems().add(translationPane);
+
+        BorderPane root = new BorderPane();
+        root.setCenter(splitPane);
+
+        StackPane bottomPane = new StackPane();
+        closeButton = new Button(LabelGrabber.INSTANCE.getLabel("close.button"));
+        StackPane.setMargin(closeButton, new Insets(10));
+        closeButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent t) {
+                hide();
+            }
+        });
+        bottomPane.getChildren().add(closeButton);
+        root.setBottom(bottomPane);
+        setScene(new Scene(root));
+        setWidth(800);
+        setHeight(600);
+    }
+
+    /**
+     * Set (update) the default lyrics on this dialog.
+     *
+     * @param lyrics the lyrics to show in the default panel.
+     */
+    public void setDefaultLyrics(String lyrics) {
+        defaultLyricsArea.setText(lyrics);
+    }
+
+    /**
+     * Clear any lyrics from this dialog (default and translations.)
+     */
+    public void clearSong() {
+        tabPane.getTabs().clear();
+        defaultLyricsArea.clear();
+    }
+
+    /**
+     * Set the song to display in this panel - updates the translations and the
+     * default lyrics view.
+     *
+     * @param song the song to display.
+     */
+    public void setSong(SongDisplayable song) {
+        tabPane.getTabs().clear();
+        defaultLyricsArea.setText(song.getLyrics(false, false));
+        if (song.getTranslations() != null) {
+            for (Entry<String, String> translation : song.getTranslations().entrySet()) {
+                tabPane.getTabs().add(new TranslateTab(translation.getKey(), translation.getValue()));
+            }
+        }
+    }
+
+    /**
+     * Get the translations as a hashmap from this dialog.
+     *
+     * @return the translations, with the name as the key and the lyrics as the
+     * value.
+     */
+    public HashMap<String, String> getTranslations() {
+        HashMap<String, String> ret = new HashMap<>();
+        for (Tab tab : tabPane.getTabs()) {
+            if (tab instanceof TranslateTab) {
+                TranslateTab ttab = (TranslateTab) tab;
+                ret.put(ttab.getName(), ttab.getLyrics());
+            } else {
+                LOGGER.log(Level.WARNING, "Non-translate tab!");
+            }
+        }
+        return ret;
+    }
+}
