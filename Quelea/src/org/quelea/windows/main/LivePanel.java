a4925465c2a28821af9673cecdf5025f4b8f299a
Ben Goodwin <beng92@users.noreply.github.com>
2014-09-14 19:58:25 +0100

    Added functionality for PageDown and PageUp to advance slides *except* when the currently active box is the database box, or type-able boxes (such as search, bible passage selection)

--- Quelea/src/org/quelea/windows/main/LivePanel.java
+++ Quelea/src/org/quelea/windows/main/LivePanel.java
@@ -28,6 +28,7 @@ import javafx.scene.control.ToolBar;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
