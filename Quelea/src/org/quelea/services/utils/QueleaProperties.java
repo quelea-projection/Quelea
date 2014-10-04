76f4b78eb8bf38693409da23fa77b6b069e3dfe8
Michael Berry <berry120@gmail.com>
2014-10-04 16:24:44 +0100

    Remove beta from version

--- Quelea/src/org/quelea/services/utils/QueleaProperties.java
+++ Quelea/src/org/quelea/services/utils/QueleaProperties.java
@@ -40,7 +40,7 @@ import org.quelea.services.languages.spelling.DictionaryManager;
  */
 public final class QueleaProperties extends Properties {
 
-    public static final Version VERSION = new Version("2014.1", "beta");
+    public static final Version VERSION = new Version("2014.1", "");
     private static final QueleaProperties INSTANCE = new QueleaProperties();
 //    private static final Logger LOGGER = LoggerUtils.getLogger();
 
