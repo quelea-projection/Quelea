db4193170f6c6e27199f424d9b1a0e776db467bf
Ben Goodwin <beng92@users.noreply.github.com>
2014-10-05 00:08:12 +0100

    Removed duplicate and unused translations

---
+++ Quelea/src/org/quelea/server/RCHandler.java
@@ -0,0 +1,185 @@
+/* 
+ * This file is part of Quelea, free projection software for churches.
+ * 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.quelea.server;
+
+import java.util.ArrayList;
+import javafx.application.Platform;
+import org.quelea.services.languages.LabelGrabber;
+import org.quelea.services.utils.QueleaProperties;
+import org.quelea.windows.main.MainPanel;
+import org.quelea.windows.main.QueleaApp;
+import org.quelea.windows.multimedia.VLCWindow;
+
+/**
+ * Handles the RemoteControlServer commands.
+ *
+ * @author Ben Goodwin
+ */
+public class RCHandler {
+
+    private static ArrayList<String> devices = new ArrayList<String>();
+
+    public static void logo() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().toggleLogo();
+            }
+        });
+    }
+
+    public static void black() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().toggleBlack();
+            }
+        });
+    }
+
+    public static void clear() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().toggleClear();
+            }
+        });
+
+    }
+
+    public static void next() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().advance();
+            }
+        });
+    }
+
+    public static void prev() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().previous();
+            }
+        });
+
+    }
+
+    public static void nextItem() {
+        final MainPanel p = QueleaApp.get().getMainWindow().getMainPanel();
+        int current = p.getSchedulePanel().getScheduleList().getItems().indexOf(p.getLivePanel().getDisplayable());
+        current++;
+        if (current < p.getSchedulePanel().getScheduleList().getItems().size()) {
+            p.getPreviewPanel().setDisplayable(p.getSchedulePanel().getScheduleList().getItems().get(current), 0);
+        } else {
+            p.getPreviewPanel().setDisplayable(p.getSchedulePanel().getScheduleList().getItems().get(p.getSchedulePanel().getScheduleList().getItems().size() - 1), 0);
+        }
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                p.getPreviewPanel().goLive();
+            }
+        });
+    }
+
+    public static void prevItem() {
+        final MainPanel p = QueleaApp.get().getMainWindow().getMainPanel();
+        int current = p.getSchedulePanel().getScheduleList().getItems().indexOf(p.getLivePanel().getDisplayable());
+        current--;
+        if (current > 0) {
+            p.getPreviewPanel().setDisplayable(p.getSchedulePanel().getScheduleList().getItems().get(current), 0);
+        } else {
+            p.getPreviewPanel().setDisplayable(p.getSchedulePanel().getScheduleList().getItems().get(0), 0);
+        }
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                p.getPreviewPanel().goLive();
+            }
+        });
+    }
+
+    public static int currentLyricSection() {
+        return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getLyricsPanel().getCurrentIndex();
+    }
+
+    public static void setLyrics(final String index) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getLyricsPanel().select(Integer.parseInt(index.substring(2)));
+            }
+        });
+    }
+
+    public static boolean authenticate(final String password) {
+        return password.equals(QueleaProperties.get().getRemoteControlPassword());
+    }
+
+    public static void addDevice(String ip) {
+        devices.add(ip);
+    }
+
+    public static boolean isLoggedOn(String ip) {
+        boolean found = false;
+        for (String s : devices) {
+            if (s.equals(ip)) {
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    public static void logout(String ip) {
+        devices.remove(ip);
+    }
+
+    public static void logAllOut() {
+        devices.clear();
+    }
+
+    public static boolean getLogo() {
+        return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getLogoed();
+    }
+
+    public static boolean getBlack() {
+        return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getBlacked();
+    }
+
+    public static boolean getClear() {
+        return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getCleared();
+    }
+
+    public static String videoStatus() {
+        if (VLCWindow.INSTANCE.isPlaying()) {
+            return LabelGrabber.INSTANCE.getLabel("pause");
+        } else {
+            return LabelGrabber.INSTANCE.getLabel("play");
+        }
+    }
+    
+    public static void play() {
+        if(VLCWindow.INSTANCE.isPlaying()) {
+            VLCWindow.INSTANCE.pause();
+        }
+        else {
+            VLCWindow.INSTANCE.play();
+        }
+    }
+}
---
+++ Quelea/src/org/quelea/windows/main/actionhandlers/EditBibleThemeScheduleActionHandler.java
@@ -0,0 +1,103 @@
+/* 
+ * This file is part of Quelea, free projection software for churches.
+ * 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.quelea.windows.main.actionhandlers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.quelea.data.displayable.BiblePassage;
+import org.quelea.data.displayable.TextSection;
+import org.quelea.services.languages.LabelGrabber;
+import org.quelea.windows.main.QueleaApp;
+import org.quelea.windows.newsong.ThemePanel;
+
+/**
+ * Called when the current song in the schedule should be edited.
+ *
+ * @author Ben
+ */
+public class EditBibleThemeScheduleActionHandler implements EventHandler<ActionEvent> {
+
+    /**
+     * Edit the currently selected song in the library.
+     *
+     * @param t the action event.
+     */
+    @Override
+    public void handle(ActionEvent t) {
+        final BiblePassage selected = (BiblePassage) QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().getSelectionModel().getSelectedItem();
+        TextArea wordsArea = new TextArea();
+        wordsArea.setText(selected.getSections()[0].toString());
+        Button confirmButton = new Button(LabelGrabber.INSTANCE.getLabel("ok.button"), new ImageView(new Image("file:icons/tick.png")));
+        Button cancelButton = new Button(LabelGrabber.INSTANCE.getLabel("cancel.button"), new ImageView(new Image("file:icons/cross.png")));
+        final Stage s = new Stage();
+        s.initModality(Modality.APPLICATION_MODAL);
+        s.initOwner(QueleaApp.get().getMainWindow());
+        s.resizableProperty().setValue(false);
+        final BorderPane bp = new BorderPane();
+        final ThemePanel tp = new ThemePanel(wordsArea, confirmButton);
+        tp.setPrefSize(500, 500);
+        tp.setTheme(selected.getTheme());
+        confirmButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (tp.getTheme() != null) {
+                    selected.setTheme(tp.getTheme());
+                    tp.updateTheme(false);
+                    for (TextSection ts : selected.getSections()) {
+                        ts.setTheme(tp.getTheme());
+                    }
+                    QueleaApp.get().getMainWindow().getMainPanel().getPreviewPanel().refresh();
+                }
+                s.hide();
+            }
+        });
+        cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                s.hide();
+            }
+        });
+        bp.setCenter(tp);
+
+        HBox hb = new HBox(10);
+        hb.setPadding(new Insets(10));
+        BorderPane.setAlignment(hb, Pos.CENTER);
+        hb.setAlignment(Pos.CENTER);
+        hb.getChildren().addAll(confirmButton, cancelButton);
+        bp.setBottom(hb);
+
+        s.setScene(new Scene(bp));
+        s.setMinHeight(600);
+        s.setMinWidth(250);
+        s.showAndWait();
+
+    }
+
+}
