649a91fa38e81ad75453feddcc98e436326c8cf3
Michael Berry <berry120@gmail.com>
2014-10-19 23:29:30 +0100

    Saved youtube videos now have title in schedules

---
+++ Quelea/src/org/quelea/extras/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The org.quelea.extras package isn't used by the core of Quelea, but instead
+ * contains other small, self-contained programs of interest (for example
+ * displaying a chart of number of invocations over time.) Nothing that Quelea
+ * itself depends on should be placed here.
+ */
+package org.quelea.extras;
