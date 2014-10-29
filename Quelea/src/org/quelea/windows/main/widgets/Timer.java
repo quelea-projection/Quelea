047825c1ba2bf68d17cf25da2a4a94debd19b679
Ben Goodwin <beng92@users.noreply.github.com>
2014-10-29 15:26:11 +0000

    Lots and lots more work done to Timers. Now supports complete theme editing, pretext, posttext, a slightly nicer UI. Still needs work, especially for stageview, but we're almost there!

--- Quelea/src/org/quelea/windows/main/widgets/Timer.java
+++ Quelea/src/org/quelea/windows/main/widgets/Timer.java
@@ -21,11 +21,14 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
+import org.quelea.data.ThemeDTO;
 import org.quelea.services.utils.QueleaProperties;
 
 /**
@@ -40,29 +43,38 @@ public class Timer extends Text {
     private boolean paused;
     private boolean reset;
     private Timeline timeline;
+    private String pretext = "";
+    private String posttext = "";
+    private Pos textPosition;
+    private ThemeDTO theme;
 
     public Timer(int minutes, int seconds) {
         this.minutes = minutes;
         this.seconds = seconds;
-        setFontSize(150);
-        setFill(QueleaProperties.get().getStageLyricsColor());
         bindToTime();
         setOpacity(1);
         paused = false;
+    }
+
+    public Timer(int minutes, int seconds, String pretext, String posttext) {
+        this(minutes, seconds);
+        this.pretext = pretext;
+        this.posttext = posttext;
     }
 
     public Timer(int seconds) {
         this((int) Math.floor(seconds / 60), seconds % 60);
     }
 
-    public void setFontSize(double fontSize) {
-        setFont(Font.font("Noto Sans", FontWeight.BOLD, FontPosture.REGULAR, fontSize));
-        setFill(QueleaProperties.get().getStageChordColor());
+    public Timer(int seconds, String pretext, String posttext) {
+        this(seconds);
+        this.pretext = pretext;
+        this.posttext = posttext;
     }
 
     private void bindToTime() {
         int starttime = minutes * 60 + seconds;
-        setText((minutes > 9 ? "" : "0") + minutes + ":" + (seconds > 9 ? "" : "0") + seconds);
+        setText(pretext + (minutes > 9 ? "" : "0") + minutes + ":" + (seconds > 9 ? "" : "0") + seconds + posttext);
         timeline = new Timeline(
                 new KeyFrame(Duration.seconds(1),
                         new EventHandler<ActionEvent>() {
@@ -79,7 +91,7 @@ public class Timer extends Text {
                                 }
                                 int remaining = starttime - count;
                                 if (remaining >= 0) {
-                                    setText(((remaining / 60) > 9 ? "" : "0") + (int) Math.floor(remaining / 60) + ":" + ((remaining % 60) > 9 ? "" : "0") + remaining % 60);
+                                    setText(pretext + ((remaining / 60) > 9 ? "" : "0") + (int) Math.floor(remaining / 60) + ":" + ((remaining % 60) > 9 ? "" : "0") + remaining % 60 + posttext);
                                 }
                             }
                         }
@@ -89,32 +101,13 @@ public class Timer extends Text {
         timeline.play();
     }
 
-    /**
-     * Creates a string left padded to the specified width with the supplied
-     * padding character.
-     * <p>
-     * @param fieldWidth the length of the resultant padded string.
-     * @param padChar a character to use for padding the string.
-     * @param s the string to be padded.
-     * @return the padded string.
-     */
-    private static String pad(int fieldWidth, char padChar, String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = s.length(); i < fieldWidth; i++) {
-            sb.append(padChar);
-        }
-        sb.append(s);
-
-        return sb.toString();
-    }
-
     public void pause() {
         paused = true;
     }
 
     public void play() {
         if (timeline.getStatus() != Timeline.Status.RUNNING) {
-            setText((minutes > 9 ? "" : "0") + minutes + ":" + (seconds > 9 ? "" : "0") + seconds);
+            setText(pretext + (minutes > 9 ? "" : "0") + minutes + ":" + (seconds > 9 ? "" : "0") + seconds + posttext);
             timeline.play();
         }
         paused = false;
@@ -128,5 +121,43 @@ public class Timer extends Text {
         reset = true;
         timeline.stop();
         setText("");
+    }
+
+    @Override
+    public String toString() {
+        return pretext + (minutes > 9 ? "" : "0") + minutes + ":" + (seconds > 9 ? "" : "0") + seconds + posttext;
+    }
+
+    public void setTheme(ThemeDTO theme) {
+        this.theme = theme;
+        this.setEffect(theme.getShadow().getDropShadow());
+        this.setFont(theme.getFont());
+        this.setFill(theme.getFontPaint());
+        this.textPosition = DisplayPositionSelector.getPosFromIndex(theme.getTextPosition());
+        this.setTextAlignment(alignmentFromIndex(theme.getTextAlignment()));
+    }
+
+    public Pos getTextPosition() {
+        return textPosition;
+    }
+
+    public void setFontSize(double pickFontSize) {
+        setFont(Font.font(theme.getFont().getFamily(),
+                theme.isBold() ? FontWeight.BOLD : FontWeight.NORMAL,
+                theme.isItalic() ? FontPosture.ITALIC : FontPosture.REGULAR,
+                pickFontSize));
+
+    }
+
+    private TextAlignment alignmentFromIndex(int index) {
+        switch (index) {
+            case 0:
+                return TextAlignment.LEFT;
+            case 1:
+                return TextAlignment.CENTER;
+            case 2:
+                return TextAlignment.RIGHT;
+        }
+        return TextAlignment.CENTER;
     }
 }
