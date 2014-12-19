b49cddc465a60995b30c1e55e87de15d880ba76b
Michael Berry <berry120@gmail.com>
2014-12-19 01:11:51 +0000

    Remove unused import

--- Quelea/src/org/quelea/windows/main/Main.java
+++ Quelea/src/org/quelea/windows/main/Main.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
