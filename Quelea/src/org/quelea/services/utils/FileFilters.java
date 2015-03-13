65d4a14d45e2bdd5492c40c13d56057ed4f66746
Ben Goodwin <beng92@users.noreply.github.com>
2015-03-13 21:02:01 +0000

    Added EpicWorship importer

--- Quelea/src/org/quelea/services/utils/FileFilters.java
+++ Quelea/src/org/quelea/services/utils/FileFilters.java
@@ -108,6 +108,10 @@ public final class FileFilters {
      * The file filter used for Songs.MB (the easyworship database.)
      */
     public static final FileChooser.ExtensionFilter SONGS_MB = new FileChooser.ExtensionFilter(LabelGrabber.INSTANCE.getLabel("filefilters.description.songs.mb") + " (Songs.MB)", "Songs.MB");
+    /**
+     * The file filter used for *.epc (the epicworship songpack.)
+     */
+    public static final FileChooser.ExtensionFilter EPC = new FileChooser.ExtensionFilter(LabelGrabber.INSTANCE.getLabel("filefilters.description.epc") + " (*.epc)", "*.epc");
 
     /**
      * No instantiation for me thanks.
