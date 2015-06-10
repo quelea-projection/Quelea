a5c0b98d56c85440ef28a478ec26ea78be264848
Ben Goodwin <beng92@users.noreply.github.com>
2015-06-10 22:10:10 +0100

    Added more bible remote control features for Arvid

--- Quelea/src/org/quelea/server/RCHandler.java
+++ Quelea/src/org/quelea/server/RCHandler.java
@@ -26,6 +26,7 @@ import java.util.Arrays;
 import java.util.TreeSet;
 import java.util.regex.Pattern;
 import javafx.application.Platform;
+import org.quelea.data.bible.Bible;
 import org.quelea.data.bible.BibleBook;
 import org.quelea.data.db.SongManager;
 import org.quelea.data.displayable.BiblePassage;
@@ -46,31 +47,31 @@ import org.quelea.windows.multimedia.VLCWindow;
  * @author Ben Goodwin
  */
 public class RCHandler {
-    
+
     private static final ArrayList<String> devices = new ArrayList<>();
-    
+
     public static void logo() {
         Platform.runLater(QueleaApp.get().getMainWindow().getMainPanel().getLivePanel()::toggleLogo);
     }
-    
+
     public static void black() {
         Platform.runLater(QueleaApp.get().getMainWindow().getMainPanel().getLivePanel()::toggleBlack);
     }
-    
+
     public static void clear() {
         Platform.runLater(QueleaApp.get().getMainWindow().getMainPanel().getLivePanel()::toggleClear);
-        
+
     }
-    
+
     public static void next() {
         Platform.runLater(QueleaApp.get().getMainWindow().getMainPanel().getLivePanel()::advance);
     }
-    
+
     public static void prev() {
         Platform.runLater(QueleaApp.get().getMainWindow().getMainPanel().getLivePanel()::previous);
-        
+
     }
-    
+
     public static void nextItem() {
         Platform.runLater(() -> {
             final MainPanel p = QueleaApp.get().getMainWindow().getMainPanel();
@@ -85,7 +86,7 @@ public class RCHandler {
             p.getPreviewPanel().goLive();
         });
     }
-    
+
     public static void prevItem() {
         Platform.runLater(() -> {
             final MainPanel p = QueleaApp.get().getMainWindow().getMainPanel();
@@ -100,26 +101,26 @@ public class RCHandler {
             p.getPreviewPanel().goLive();
         });
     }
-    
+
     public static int currentLyricSection() {
         return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getLyricsPanel().getCurrentIndex();
     }
-    
+
     public static void setLyrics(final String index) {
         Platform.runLater(() -> {
             int num = Integer.parseInt(index.split("section")[1]);
             QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getLyricsPanel().select(num);
         });
     }
-    
+
     public static boolean authenticate(final String password) {
         return password.equals(QueleaProperties.get().getRemoteControlPassword());
     }
-    
+
     public static void addDevice(String ip) {
         devices.add(ip);
     }
-    
+
     public static boolean isLoggedOn(String ip) {
         boolean found = false;
         for (String s : devices) {
@@ -129,27 +130,27 @@ public class RCHandler {
         }
         return found;
     }
-    
+
     public static void logout(String ip) {
         devices.remove(ip);
     }
-    
+
     public static void logAllOut() {
         devices.clear();
     }
-    
+
     public static boolean getLogo() {
         return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getLogoed();
     }
-    
+
     public static boolean getBlack() {
         return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getBlacked();
     }
-    
+
     public static boolean getClear() {
         return QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getCleared();
     }
-    
+
     public static String videoStatus() {
         if (VLCWindow.INSTANCE.isPlaying()) {
             return LabelGrabber.INSTANCE.getLabel("pause");
@@ -157,7 +158,7 @@ public class RCHandler {
             return LabelGrabber.INSTANCE.getLabel("play");
         }
     }
-    
+
     public static void play() {
         if (VLCWindow.INSTANCE.isPlaying()) {
             VLCWindow.INSTANCE.pause();
@@ -165,11 +166,11 @@ public class RCHandler {
             VLCWindow.INSTANCE.play();
         }
     }
-    
+
     public static String schedule() {
         Displayable preview = QueleaApp.get().getMainWindow().getMainPanel().getPreviewPanel().getDisplayable();
         Displayable live = QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getDisplayable();
-        
+
         String display = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n</head>\n";
         for (int i = 0; i < QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().getItems().size(); i++) {
             Displayable d = ((Displayable) QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().getItems().get(i));
@@ -191,7 +192,7 @@ public class RCHandler {
         display += "</html>";
         return display;
     }
-    
+
     public static String databaseSearch(HttpExchange he) throws UnsupportedEncodingException, IOException {
         String searchString;
         if (he.getRequestURI().toString().contains("/search/")) {
@@ -206,17 +207,17 @@ public class RCHandler {
                     song.setLastSearch(searchString);
                     songs.add(song);
                 }
-                
+
                 SongDisplayable[] lyricSongs = SongManager.get().getIndex().filter(searchString, SongSearchIndex.FilterType.BODY);
                 for (SongDisplayable song : lyricSongs) {
                     song.setLastSearch(null);
                     songs.add(song);
                 }
-                
+
                 SongDisplayable[] authorSongs = SongManager.get().getIndex().filter(searchString, SongSearchIndex.FilterType.AUTHOR);
                 songs.addAll(Arrays.asList(authorSongs));
             }
-            
+
             StringBuilder response = new StringBuilder();
             response.append("<!DOCTYPE html><html>");
             response.append("<head><meta charset=\"UTF-8\"></head>");
@@ -231,7 +232,7 @@ public class RCHandler {
             return "";
         }
     }
-    
+
     public static String addSongToSchedule(HttpExchange he) {
         String songIDString;
         long songID;
@@ -239,16 +240,16 @@ public class RCHandler {
             songIDString = he.getRequestURI().toString().split("/add/", 2)[1];
             songID = Long.parseLong(songIDString);
             SongDisplayable sd = SongManager.get().getIndex().getByID(songID);
-            
-            Platform.runLater(() -> {
+
+            Utils.fxRunAndWait(() -> {
                 QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().add(sd);
             });
-            
+
             return LabelGrabber.INSTANCE.getLabel("rcs.add.success");
         }
         return LabelGrabber.INSTANCE.getLabel("rcs.add.failed");
     }
-    
+
     public static String songDisplay(HttpExchange he) {
         String songIDString;
         long songID;
@@ -270,39 +271,51 @@ public class RCHandler {
         }
         return "";
     }
-    
+
     public static String addBiblePassage(HttpExchange he) throws UnsupportedEncodingException {
         String searchString;
-        if (he.getRequestURI().toString().contains("/bible/")) {
+        if (he.getRequestURI().toString().contains("/addbible/")) {
             String uri = URLDecoder.decode(he.getRequestURI().toString(), "UTF-8");
-            searchString = uri.split("/bible/")[1];
-            String book = searchString.split("/")[0];
-            String cv = searchString.split("/")[1];
-            System.out.println(book + " " + cv);
+            searchString = uri.split("/addbible/")[1];
+            String translation = searchString.split("/")[0];
+            String book = searchString.split("/")[1];
+            String cv = searchString.split("/")[2];
             String error = LabelGrabber.INSTANCE.getLabel("rcs.add.bible.error").replace("$1", book + " " + cv);
             final LibraryBiblePanel lbp = QueleaApp.get().getMainWindow().getMainPanel().getLibraryPanel().getBiblePanel();
             boolean success = false;
+            for (int i = 0; i < lbp.getBibleSelector().getItems().size(); i++) {
+                if (lbp.getBibleSelector().getItems().get(i).getBibleName().equalsIgnoreCase(translation)) {
+                    final int j = i;
+                    Utils.fxRunAndWait(() -> {
+                        lbp.getBibleSelector().selectionModelProperty().get().clearAndSelect(j);
+                    });
+                    success = true;
+                }
+            }
+            if (!success) {
+                return error;
+            }
+            success = false;
             for (int i = 0; i < lbp.getBookSelector().getItems().size(); i++) {
                 if (lbp.getBookSelector().getItems().get(i).getBookName().equalsIgnoreCase(book)) {
                     final int j = i;
-                    Platform.runLater(() -> {
+                    Utils.fxRunAndWait(() -> {
                         lbp.getBookSelector().selectionModelProperty().get().clearAndSelect(j);
                     });
                     success = true;
                 }
             }
-            System.out.println(success);
             if (!success) {
                 return error;
             }
-            
+
             int before = QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().getItems().size();
             Utils.fxRunAndWait(() -> {
                 lbp.getPassageSelector().setText(cv);
                 lbp.getAddToSchedule().fire();
             });
             int after = QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().getItems().size();
-            
+
             if (after > before) {
                 return LabelGrabber.INSTANCE.getLabel("rcs.add.success");
             } else {
@@ -311,5 +324,45 @@ public class RCHandler {
         } else {
             return "";
         }
+    }
+
+    public static String listBibleTranslations(HttpExchange he) {
+        StringBuilder ret = new StringBuilder();
+        final LibraryBiblePanel lbp = QueleaApp.get().getMainWindow().getMainPanel().getLibraryPanel().getBiblePanel();
+        for (Bible b : lbp.getBibleSelector().getItems()) {
+            if (b.getBibleName().equals(QueleaProperties.get().getDefaultBible())) {
+                ret.append("*");
+            }
+            ret.append(b.getBibleName()).append("\n");
+        }
+        return ret.toString();
+    }
+
+    public static String listBibleBooks(HttpExchange he) throws UnsupportedEncodingException {
+        String searchString;
+        if (he.getRequestURI().toString().contains("/books/")) {
+            String uri = URLDecoder.decode(he.getRequestURI().toString(), "UTF-8");
+            searchString = uri.split("/books/")[1];
+            final LibraryBiblePanel lbp = QueleaApp.get().getMainWindow().getMainPanel().getLibraryPanel().getBiblePanel();
+            boolean success = false;
+            for (int i = 0; i < lbp.getBibleSelector().getItems().size(); i++) {
+                if (lbp.getBibleSelector().getItems().get(i).getBibleName().equalsIgnoreCase(searchString)) {
+                    final int j = i;
+                    Utils.fxRunAndWait(() -> {
+                        lbp.getBibleSelector().selectionModelProperty().get().clearAndSelect(j);
+                    });
+                    success = true;
+                }
+            }
+            if (!success) {
+                return "";
+            }
+            StringBuilder ret = new StringBuilder();
+            for (BibleBook bb : lbp.getBookSelector().getItems()) {
+                ret.append(bb.getBookName()).append("\n");
+            }
+            return ret.toString();
+        }
+        return "";
     }
 }
