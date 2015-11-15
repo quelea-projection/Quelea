809378233842689943d4ff87dcaed80a1c00e5b3
Ben Goodwin <beng92@users.noreply.github.com>
2015-11-15 22:45:54 +0000

    Fix for ipv6 showing instead of ipv4 in server settings 3

--- Quelea/src/org/quelea/windows/options/ServerSettingsPanel.java
+++ Quelea/src/org/quelea/windows/options/ServerSettingsPanel.java
@@ -27,6 +27,7 @@ import java.awt.Desktop;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -382,7 +383,7 @@ public class ServerSettingsPanel extends GridPane implements PropertyPanel {
                 if (current_addr instanceof Inet6Address && v6address == null) {
                     LOGGER.log(Level.INFO, "Storing v6 address, no v4 found yet: {0}", v6address);
                     v6address = current_addr.getHostAddress();
-                } else if (!(current_addr instanceof Inet6Address)) {
+                } else if (current_addr instanceof Inet4Address) {
                     LOGGER.log(Level.INFO, "Found v4: {0}", current_addr.getHostAddress());
                     return current_addr.getHostAddress();
                 }
