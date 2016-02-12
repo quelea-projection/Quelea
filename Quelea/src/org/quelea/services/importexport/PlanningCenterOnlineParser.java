b701f3369740936137c7f13dbb7a4367b666e3e7
Fabian <supagu@gmail.com>
2016-02-12 21:27:19 +1030

    -PCO login now working correctly -Initial plan information being retreived from PCO

--- Quelea/src/org/quelea/services/importexport/PlanningCenterOnlineParser.java
+++ Quelea/src/org/quelea/services/importexport/PlanningCenterOnlineParser.java
@@ -17,14 +17,35 @@
 package org.quelea.services.importexport;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
+import java.net.CookieStore;
+import java.net.HttpCookie;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  *
@@ -34,14 +55,31 @@ import java.nio.charset.StandardCharsets;
 
 public class PlanningCenterOnlineParser {
     
-    public PlanningCenterOnlineParser() {
+    private final DefaultHttpClient httpClient;
+    private final BasicCookieStore cookieStore;
+    private final HttpContext httpContext;
     
+    public PlanningCenterOnlineParser() {
+        
+        // need to handle cookies
+        CookieManager cm = ((CookieManager)CookieHandler.getDefault());
+        if (cm == null) {
+            CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ) );
+        }
+        else {
+            cm.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        }
+        
+        httpClient = new DefaultHttpClient();
+        cookieStore = new BasicCookieStore();
+        httpContext = new BasicHttpContext();
+        httpContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
     }
     
-    public static boolean login(String email, String password) {
+    public boolean login(String email, String password) {
         String args = String.format("email=%s&password=%s&submit=login", email, password);
         String url = "https://accounts.planningcenteronline.com/login";
-        String result = post(url, args);
+        String result = post(url, args, email, password);
         if (result == null) {
             return false;
         }
@@ -54,57 +92,128 @@ public class PlanningCenterOnlineParser {
         return true;
     }
     
-    private static String post(String urlString, String urlParameters) {
-        
-        // ensure cookie handler setup
-        if (CookieHandler.getDefault() != null) {
-            CookieHandler.setDefault( new CookieManager( null, CookiePolicy.ACCEPT_ALL ) );
-        }
-        
-        BufferedReader reader = null;
-        try {
-            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-            int    postDataLength = postData.length;
-
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setConnectTimeout(5000);
-            conn.setReadTimeout(10000);
+    private String post(String urlString, String urlParameters, String email, String password) {
+        try {         
+            HttpPost httpost = new HttpPost(urlString);
+            List<NameValuePair> nvps = new ArrayList <NameValuePair>();
+            nvps.add(new BasicNameValuePair("email", email));
+            nvps.add(new BasicNameValuePair("password", password));
+            httpost.setEntity(new UrlEncodedFormEntity(nvps));
             
-            conn.setDoOutput( true );
-            conn.setInstanceFollowRedirects( false );
-            conn.setRequestMethod( "POST" );
-            conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded"); 
-            conn.setRequestProperty( "charset", "utf-8");
-            conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-            conn.setUseCaches( false );
-            try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
-               wr.write( postData );
+            HttpResponse response = httpClient.execute(httpost, httpContext);
+                       
+            Header[] cookieList = response.getHeaders("Set-Cookie");
+            for (Header cookieHeader : cookieList) {
+                String cookieStr = cookieHeader.getValue();
+                
+                HttpCookie httpCookie = HttpCookie.parse(cookieStr).get(0);
+                BasicClientCookie cookie = new BasicClientCookie(httpCookie.getName(), httpCookie.getValue());
+                cookie.setPath(httpCookie.getPath());
+                cookie.setDomain(httpCookie.getDomain());
+                
+                Date today = new Date();
+                Date tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24));
+                cookie.setExpiryDate(tomorrow);
+                cookieStore.addCookie(cookie);
             }
-
-            StringBuilder strBuilder = new StringBuilder();
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"))) {
-                String str;
-                while ((str = in.readLine()) != null) {
-                    strBuilder.append(str).append("\n");
-                }
-                String resultStr = strBuilder.toString();
-                //String[] jsonMap = json.split(",");
-                /*
-                for(String s : jsonMap) {
-                    if(s.split(": ")[0].contains("country") && s.split(": ")[1].equals("\"US\"")) {
-                        useUK = false;
-                        LOGGER.log(Level.INFO, "Using US version of weareworship.com");
+            
+            HttpEntity entity = response.getEntity();
+            String text = EntityUtils.toString(entity);
+            /*
+            if (response1.getStatusLine().getStatusCode() == 302) {
+                Header[] locationList = response1.getHeaders("Location");
+                if (locationList.length > 0)
+                {
+                    String newUrl = locationList[0].getValue();
+                    
+                    try {
+                        HttpGet httget = new HttpGet(newUrl);
+                        response1 = httpClient.execute(httget, httpContext);
+                        
+                        entity = response1.getEntity();
+                        text = EntityUtils.toString(entity);
+                    } catch (Exception e) {
+                        int nothing = 0;
+                        ++nothing;
                     }
-                }*/
-                return resultStr;
-            } catch (Exception e) {
+                }
             }
-        } catch (Exception e) {
+            */
+            
+            return text;
+        } 
+        catch (Exception e) {
         }
         
         return null;
     }
-         
+    
+    private String get(String urlString) {
+        try {
+            HttpGet httget = new HttpGet(urlString);
+            HttpResponse response = httpClient.execute(httget, httpContext);
+
+            HttpEntity entity = response.getEntity();
+            String text = EntityUtils.toString(entity);
+            return text;
+        } catch (Exception e) {
+            return null;
+        }
+        
+        /*
+        // debug cookies
+        CookieManager cm = ((CookieManager)CookieHandler.getDefault());
+        CookieStore cookies = cm.getCookieStore();
+        
+        
+        try {
+            StringBuilder content = new StringBuilder();
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            
+            int response = conn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_MOVED_PERM || response == HttpURLConnection.HTTP_MOVED_TEMP) {
+                url = new URL(conn.getHeaderField("Location"));
+            }
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), "UTF-8"))) {
+                String str;
+                while ((str = in.readLine()) != null) {
+                    content.append(str).append("\n");
+                }
+            }
+            return content.toString();
+        } catch (Exception ex) {
+            return null;
+        }*/
+    }
+    
+    private JSONObject getJson(String urlString) {
+        try {
+            String jsonStr = get(urlString);
+            
+            // fix up arrays JSONParser wont handle
+            if (jsonStr.startsWith("[") && jsonStr.endsWith("]"))
+            {
+                jsonStr = "{\"array\":" + jsonStr + "}";
+            }
+            
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(jsonStr);
+            return json;
+        }
+        catch(Exception e) {
+            return null;
+        }
+    }
+    
+    // Contains organisation data - service types
+    public JSONObject organisation() {        
+        return getJson("https://services.planningcenteronline.com/organization.json");
+    }
+    
+    // Contains all plans for a certain service type
+    public JSONObject serviceTypePlans(Long serviceTypeId) {
+        return getJson("https://planningcenteronline.com/service_types/" + serviceTypeId + "/plans.json");
+    }
     
 }
