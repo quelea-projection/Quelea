1974b0a657265b40525891fd45764e10c5110c12
Fabian <supagu@gmail.com>
2016-02-14 21:24:12 +1030

    Custom slide support

--- Quelea/src/org/quelea/services/importexport/PlanningCenterOnlinePlanDialog.java
+++ Quelea/src/org/quelea/services/importexport/PlanningCenterOnlinePlanDialog.java
@@ -17,17 +17,24 @@
  */
 package org.quelea.services.importexport;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.Pane;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.quelea.data.displayable.TextSection;
+import org.quelea.data.displayable.TextSlides;
 import org.quelea.services.languages.LabelGrabber;
+import org.quelea.windows.main.QueleaApp;
 
 /**
  *
@@ -37,6 +44,14 @@ import org.quelea.services.languages.LabelGrabber;
 
 public class PlanningCenterOnlinePlanDialog extends Pane {
     
+    enum PlanType {
+        PlanMedia,
+        PlanSong,
+        PlanCustomSlides,
+        PlanUnknown,
+    }
+    
+    private final Map<TreeItem<String>, JSONObject> treeViewItemMap = new HashMap<TreeItem<String>, JSONObject>();
     private final PlanningCenterOnlineImportDialog importDialog;
     private final Long planId;
     
@@ -59,17 +74,34 @@ public class PlanningCenterOnlinePlanDialog extends Pane {
             loader.setResources(LabelGrabber.INSTANCE);
             Parent root = loader.load(getClass().getResourceAsStream("PlanningCenterOnlinePlanDialog.fxml"));
             getChildren().addAll(root);
-            
-            planJSON = importDialog.getParser().plan(planId);
-            update();
+            planView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+  
+            updateView();
         
         } catch (Exception e) {
             e.printStackTrace();
         }       
     }
     
+    protected PlanType getItemPlanType(JSONObject item) {
+        String itemType = (String)item.get("type");
+        if (itemType.equals("PlanMedia")) {
+            return PlanType.PlanMedia;
+        }
+        else if (itemType.equals("PlanSong")) {
+            return PlanType.PlanSong;
+        }
+        else if (itemType.equals("PlanItem") && (boolean)item.get("using_custom_slides") == true) {
+            return PlanType.PlanCustomSlides;
+        }
+        
+        return PlanType.PlanUnknown;
+    }
+            
     
-    protected void update() {
+    protected void updateView() {
+        planJSON = importDialog.getParser().plan(planId);  
+        
         planView.setShowRoot(false);
         TreeItem<String> rootTreeItem = new TreeItem<String>();
         planView.setRoot(rootTreeItem);
@@ -80,38 +112,107 @@ public class PlanningCenterOnlinePlanDialog extends Pane {
             JSONObject item = (JSONObject)itemObj;
             ++itemIndex;
             
-            String itemType = (String)item.get("type");
-            if (itemType.equals("PlanMedia")) {
-                processPlanMedia(item, rootTreeItem);
-            }
-            else if (itemType.equals("PlanSong")) {
-                processPlanSong(item, rootTreeItem);
-            }
-            else if (itemType.equals("PlanItem") && (boolean)item.get("using_custom_slides") == true) {
-                processCustomSlides(item, rootTreeItem);
+            PlanType planType = getItemPlanType(item);
+            switch (planType)
+            {
+                case PlanMedia:
+                    addToView_PlanMedia(item, rootTreeItem);
+                    break;
+                    
+                case PlanSong:
+                    addToView_PlanSong(item, rootTreeItem);
+                    break;
+                    
+                case PlanCustomSlides:
+                    addToView_CustomSlides(item, rootTreeItem);
+                    break;
+                    
+                default:
+                    break;
             }
         }
     }
     
-    protected void processPlanMedia(JSONObject item, TreeItem<String> parentTreeItem) {
+    protected void addToView_PlanMedia(JSONObject item, TreeItem<String> parentTreeItem) {
         String title = "Media: " + (String)item.get("title");
         TreeItem<String> treeItem = new TreeItem<String>(title);
         parentTreeItem.getChildren().add(treeItem);
+        treeViewItemMap.put(treeItem, item);
     }
     
-    protected void processPlanSong(JSONObject item, TreeItem<String> parentTreeItem) {
+    protected void addToView_PlanSong(JSONObject item, TreeItem<String> parentTreeItem) {
         String title = "Song: " + (String)item.get("title");
         TreeItem<String> treeItem = new TreeItem<String>(title);
         parentTreeItem.getChildren().add(treeItem);
+        treeViewItemMap.put(treeItem, item);
     }
     
-    protected void processCustomSlides(JSONObject item, TreeItem<String> parentTreeItem) {
-        String title = "Custom Slides: " + (String)item.get("title") + " - " + (String)item.get("dates");
+    protected void addToView_CustomSlides(JSONObject item, TreeItem<String> parentTreeItem) {
+        String title = "Custom Slides: " + (String)item.get("title");
         TreeItem<String> treeItem = new TreeItem<String>(title);
         parentTreeItem.getChildren().add(treeItem);
+        treeViewItemMap.put(treeItem, item);
     }
     
-    @FXML private void onImportAction(ActionEvent event) {
-        System.out.println("Do import");
+    @FXML private void onImportAllAction(ActionEvent event) {
+        List<TreeItem<String> > allTreeItems = planView.getRoot().getChildren();
+        importSelected(allTreeItems);
+    }
+    
+    @FXML private void onImportSelectedAction(ActionEvent event) {
+        List<TreeItem<String> > selectedTreeItems = planView.getSelectionModel().getSelectedItems();
+        importSelected(selectedTreeItems);
+    }
+    
+    @FXML private void onRefreshAction(ActionEvent event) {
+        updateView();
+    }
+    
+    private void importSelected(List<TreeItem<String> > selectedTreeItems) {
+        for (TreeItem<String> treeItem : selectedTreeItems) {
+            JSONObject item = treeViewItemMap.get(treeItem);
+            
+            PlanType planType = getItemPlanType(item);
+            switch (planType)
+            {
+                case PlanMedia:
+                    import_PlanMedia(item, treeItem);
+                    break;
+                    
+                case PlanSong:
+                    import_PlanSong(item, treeItem);
+                    break;
+                    
+                case PlanCustomSlides:
+                    import_CustomSlides(item, treeItem);
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+    }
+    
+    protected void import_PlanMedia(JSONObject item, TreeItem<String> treeItem) {
+    }
+    
+    protected void import_PlanSong(JSONObject item, TreeItem<String> treeItem) {
+    }
+    
+    protected void import_CustomSlides(JSONObject item, TreeItem<String> treeItem) {
+        String title = (String)item.get("title");
+        List<TextSection> textSections = new ArrayList<TextSection>();
+        
+        JSONArray customSlides = (JSONArray)item.get("custom_slides");
+        for (Object slideObj : customSlides) {
+            JSONObject slide = (JSONObject)slideObj;            
+            String body = (String)slide.get("body");
+            String[] bodyLines = body.split("\\r?\\n");
+            TextSection text = new TextSection(""/*(String)slide.get("label")*/, bodyLines, null, false);
+            textSections.add(text);
+        }
+
+        TextSlides slides = new TextSlides(title, textSections);
+        QueleaApp.get().getMainWindow().getMainPanel().getSchedulePanel().getScheduleList().add(slides);
     }
 }
