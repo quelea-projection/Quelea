e94e709bbf7bcdbac65539813eeafb862961c2d0
Arvid <Arvid@localhost>
2016-06-05 22:42:36 +0200

    Add warning to save recording before exiting

--- Quelea/src/org/quelea/windows/main/actionhandlers/ExitActionHandler.java
+++ Quelea/src/org/quelea/windows/main/actionhandlers/ExitActionHandler.java
@@ -21,9 +21,11 @@ package org.quelea.windows.main.actionhandlers;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 import org.javafx.dialog.Dialog;
 import org.quelea.data.SaveCallback;
 import org.quelea.data.Schedule;
@@ -140,6 +142,51 @@ public class ExitActionHandler implements EventHandler<ActionEvent> {
         if (QueleaApp.get().getMainWindow().getMainPanel().getLivePanel().getDisplayable() instanceof PresentationDisplayable) {
             LOGGER.log(Level.INFO, "Closing open PowerPoint presentations");
             PowerPointHandler.closePresentation();
+        }
+        
+        LOGGER.log(Level.INFO, "Checking if Quelea currently is recording audio");
+        MainToolbar toolbar = mainWindow.getMainToolbar();
+        RecordingsHandler recHandler = toolbar.getRecordButtonHandler().getRecordingsHandler();
+        if (toolbar.getRecordButtonHandler() != null && recHandler != null) {
+            if (recHandler.getIsRecording()) {
+                block = true;
+                Dialog d = Dialog.buildConfirmation(LabelGrabber.INSTANCE.getLabel("save.recording.before.exit.title"), LabelGrabber.INSTANCE.getLabel("save.recording.before.exit.message"))
+                        .addYesButton((ActionEvent t1) -> {
+                            toolbar.stopRecording();
+                        }).addNoButton((ActionEvent t1) -> {
+                            System.exit(0);
+                        }).build();
+                d.setOnCloseRequest((WindowEvent we) -> {
+                    System.exit(0);
+                });
+                Thread thread = new Thread(() -> {
+                    while (block) {
+                        try {
+                            Thread.sleep(500);
+                            if (recHandler.getFinishedSaving()) {
+                                Platform.runLater(() -> {
+                                    d.close();
+                                });
+                                if (QueleaProperties.get().getConvertRecordings()) {
+                                    boolean converting = recHandler.isConverting();
+                                    if (!converting) {
+                                        block = false;
+                                        System.exit(0);
+                                    }
+                                } else {
+                                    block = false;
+                                    System.exit(0);
+                                }
+                            }
+                        } catch (InterruptedException ex) {
+                        }
+                    }
+                });
+                thread.setDaemon(true);
+                thread.start();
+                d.showAndWait();
+                return; //Don't exit until the recording is saved and converted
+            }
         }
         System.exit(0);
     }
