d80d310b1459556ca7b2471d483404b044b4c3b4
Michael Berry <berry120@gmail.com>
2017-03-26 23:29:46 +0100

    Fix importer bug for openlyrics

--- Quelea/src/org/quelea/services/importexport/OpenLyricsParser.java
+++ Quelea/src/org/quelea/services/importexport/OpenLyricsParser.java
@@ -25,6 +25,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -56,7 +57,7 @@ public class OpenLyricsParser implements SongParser {
      */
     @Override
     public List<SongDisplayable> getSongs(File location, StatusPanel statusPanel) throws IOException {
-        final ZipFile file = new ZipFile(location);
+        final ZipFile file = new ZipFile(location, Charset.forName("Cp437"));
         List<SongDisplayable> ret = new ArrayList<>();
         try {
             final Enumeration<? extends ZipEntry> entries = file.entries();
