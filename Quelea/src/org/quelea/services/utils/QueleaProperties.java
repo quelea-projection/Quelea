fada6069a3c35f2bd710338abc5c075443fc33c3
Michael Berry <berry120@gmail.com>
2017-09-05 21:48:36 +0100

    Bump version to 2017.1

--- Quelea/src/org/quelea/services/utils/QueleaProperties.java
+++ Quelea/src/org/quelea/services/utils/QueleaProperties.java
@@ -41,7 +41,7 @@ import org.quelea.services.notice.NoticeDrawer.NoticePosition;
  */
 public final class QueleaProperties extends Properties {
 
-    public static final Version VERSION = new Version("2017.0", "");
+    public static final Version VERSION = new Version("2017.1", "");
     private static final QueleaProperties INSTANCE = new QueleaProperties();
 
     /**
