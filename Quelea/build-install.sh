6b304f96d803c20e1f73c5dd2885982a9b380397
berry120 <berry120@gmail.com>
2018-07-04 15:55:11 +0100

    Update travis

--- Quelea/build-install.sh
+++ Quelea/build-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wine "/home/michael/.wine/drive_c/Program Files/Inno Setup 5/iscc.exe" quelea.iss /O /F /q"Quelea"
-wine "/home/michael/.wine/drive_c/Program Files/Inno Setup 5/iscc.exe" quelea64.iss /O /F /q"Quelea"
+wine "/home/travis/.wine/drive_c/Program Files/Inno Setup 5/iscc.exe" quelea.iss /O /F /q"Quelea"
+wine "/home/travis/.wine/drive_c/Program Files/Inno Setup 5/iscc.exe" quelea64.iss /O /F /q"Quelea"
