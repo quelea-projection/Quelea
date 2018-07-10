79c2d5abf3e5cb82928366131388bee9e2dbb244
berry120 <berry120@gmail.com>
2018-07-11 00:05:23 +0100

    Fix launch.sh for snap

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -Xms1200m -Dfile.encoding=UTF-8 -Djavafx.embed.singleThread=true -Dprism.dirtyopts=false -jar Quelea.jar
+java -Xms1200m -Dfile.encoding=UTF-8 -Djavafx.embed.singleThread=true -Dprism.dirtyopts=false -jar Quelea.jar
