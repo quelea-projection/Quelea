b450cf5009a36b296d08243935909ace205a1e28
berry120 <berry120@gmail.com>
2018-07-12 23:25:44 +0100

    Attempted fontmetrics fix

--- Quelea/src/main/java/org/quelea/utils/FontMetricsWrapper.java
+++ Quelea/src/main/java/org/quelea/utils/FontMetricsWrapper.java
@@ -1,7 +1,9 @@
 package org.quelea.utils;
 
 import com.sun.javafx.tk.FontMetrics;
-import java.lang.reflect.Method;
+import javafx.geometry.Bounds;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -9,55 +11,62 @@ import java.lang.reflect.Method;
  */
 public class FontMetricsWrapper {
 
-    private FontMetrics metrics;
+    private FontMetricsIntl metrics;
 
     public FontMetricsWrapper(FontMetrics metrics) {
-        this.metrics = metrics;
+        this.metrics = new FontMetricsIntl(metrics.getFont());
     }
 
     public double getLineHeight() {
-        return metrics.getLineHeight();
+        return metrics.lineHeight;
     }
 
     public double computeStringWidth(String line) {
-        if (getCharWidthMethod() != null) {
-            if (line == null) {
-                return 0;
-            }
-            double ret = 0;
-            Method m = getCharWidthMethod();
-            for (char c : line.toCharArray()) {
-                ret += (float)invoke(m, metrics, c);
-            }
-            return ret;
-        }
-        else {
-            return (float)invoke(getComputeStringWidthMethod(), metrics, line);
-        }
+        return metrics.computeStringWidth(line);
     }
+//    
+//    public Object invoke(Method m, Object o, Object... args) {
+//        try {
+//            return m.invoke(o, args);
+//        }
+//        catch(Exception ex) {
+//            return null;
+//        }
+//    }
+//
+//    public Method getCharWidthMethod() {
+//        try {
+//            return FontMetrics.class.getMethod("getCharWidth", char.class);
+//        } catch (Exception ex) {
+//            return null;
+//        }
+//    }
+//
+//    public Method getComputeStringWidthMethod() {
+//        try {
+//            return FontMetrics.class.getMethod("computeStringWidth", String.class);
+//        } catch (Exception ex) {
+//            return null;
+//        }
+//    }
     
-    public Object invoke(Method m, Object o, Object... args) {
-        try {
-            return m.invoke(o, args);
-        }
-        catch(Exception ex) {
-            return null;
-        }
-    }
+    class FontMetricsIntl {
 
-    public Method getCharWidthMethod() {
-        try {
-            return FontMetrics.class.getMethod("getCharWidth", char.class);
-        } catch (Exception ex) {
-            return null;
-        }
-    }
+        final private Text internal;
+        public float ascent, descent, lineHeight;
 
-    public Method getComputeStringWidthMethod() {
-        try {
-            return FontMetrics.class.getMethod("computeStringWidth", String.class);
-        } catch (Exception ex) {
-            return null;
+        public FontMetricsIntl(Font fnt) {
+            internal = new Text();
+            internal.setFont(fnt);
+            Bounds b = internal.getLayoutBounds();
+            lineHeight = (float) b.getHeight();
+            ascent = (float) -b.getMinY();
+            descent = (float) b.getMaxY();
+        }
+
+        public float computeStringWidth(String txt) {
+            internal.setText(txt);
+            return (float) internal.getLayoutBounds().getWidth();
         }
     }
 
