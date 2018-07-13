c9fdead5818adef3343d8318f5f949034eb25094
berry120 <berry120@gmail.com>
2018-07-13 19:02:50 +0100

    Fix PDF export

--- Quelea/src/main/java/org/quelea/services/print/PDFPrinter.java
+++ Quelea/src/main/java/org/quelea/services/print/PDFPrinter.java
@@ -23,12 +23,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import javax.xml.transform.Result;
 import javax.xml.transform.Transformer;
@@ -39,6 +35,10 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
 import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.FopFactoryBuilder;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -49,15 +49,10 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class PDFPrinter {
 
-    private static Object fopFactory;
-    private static Object fopFactoryBuilderObj;
-    private static Object foUserAgent;
+    private static FopFactory fopFactory;
+    private static FopFactoryBuilder builder;
+    private static FOUserAgent foUserAgent;
     private static TransformerFactory tranFactory;
-    private static Class fopFactoryBuilderCls;
-    private static Class fopFactoryCls;
-    private static Class fopCls;
-    private static Class foUserAgentCls;
-    private static Class configurationCls;
 
     /**
      * Print a PDF file.
@@ -67,7 +62,7 @@ public class PDFPrinter {
      * @param pdfFile the file to print to.
      * @throws IOException if anything goes wrong.
      */
-    @SuppressWarnings("unchecked") 
+    @SuppressWarnings("unchecked")
     public void print(String xml, File xsltfile, File pdfFile) throws IOException {
         try {
             InputStream stream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
@@ -76,98 +71,26 @@ public class PDFPrinter {
             DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder();
             Configuration cfg = cfgBuilder.buildFromFile(new File("fopcfg.xml"));
 
-            /*
-            Yuk, I know. The issue is here that Apache FOP hasn't been updated to use PDFBox 2.x, it's stuck on 1.8.
-            We can't include both by default because that causes all sorts of classloader issues, so exclude 1.8
-            from the default classpath, load it manually and then reflect around it here.
-             */
-
-            if (fopFactoryBuilderObj == null) {
-                CustomClassLoader clsLoader = new CustomClassLoader(new java.net.URL[]{
-                    new java.net.URL("file:lib/fop-2.1.jar"),
-                    new java.net.URL("file:lib/pdfbox-app-1.8.13.jar")
-                });
-                fopFactoryBuilderCls = clsLoader.loadClass("org.apache.fop.apps.FopFactoryBuilder");
-                fopFactoryCls = clsLoader.loadClass("org.apache.fop.apps.FopFactory");
-                fopCls = clsLoader.loadClass("org.apache.fop.apps.Fop");
-                foUserAgentCls = clsLoader.loadClass("org.apache.fop.apps.FOUserAgent");
-                configurationCls = clsLoader.loadClass("org.apache.avalon.framework.configuration.Configuration");
-                fopFactoryBuilderObj = fopFactoryBuilderCls.getConstructor(URI.class).newInstance(new URI("."));
-                fopFactoryBuilderObj = fopFactoryBuilderCls.getMethod("setConfiguration", configurationCls).invoke(fopFactoryBuilderObj, cfg);
-                fopFactory = fopFactoryBuilderCls.getMethod("build").invoke(fopFactoryBuilderObj);
-                foUserAgent = fopFactoryCls.getMethod("newFOUserAgent").invoke(fopFactory);
+            if (builder == null) {
+                builder = new FopFactoryBuilder(new URI("."));
+                builder.setConfiguration(cfg);
+                fopFactory = builder.build();
+                foUserAgent = fopFactory.newFOUserAgent();
                 tranFactory = TransformerFactory.newInstance();
             }
 
             ByteArrayOutputStream outStream = new ByteArrayOutputStream();
             Transformer xslfoTransformer = tranFactory.newTransformer(transformSource);
 
-            Object fop = fopFactoryCls.getMethod("newFop", String.class, foUserAgentCls, OutputStream.class).invoke(fopFactory, "application/pdf", foUserAgent, outStream);
-            Result res = new SAXResult((DefaultHandler) fopCls.getMethod("getDefaultHandler").invoke(fop));
+            Fop fop = fopFactory.newFop("application/pdf", foUserAgent, outStream);
+            Result res = new SAXResult((DefaultHandler) fop.getDefaultHandler());
             xslfoTransformer.transform(source, res);
 
-            OutputStream out = new java.io.FileOutputStream(pdfFile);
-            out = new java.io.BufferedOutputStream(out);
-            FileOutputStream str = new FileOutputStream(pdfFile);
-            str.write(outStream.toByteArray());
-            str.close();
-            out.close();
-        } catch (IOException | ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchMethodException | SecurityException | InvocationTargetException | URISyntaxException | TransformerException | ConfigurationException | SAXException ex) {
-            throw new IOException("Error printing to PDF", ex);
-        }
-    }
-}
-
-class CustomClassLoader extends ClassLoader {
-
-    private final ChildClassLoader childClassLoader;
-
-    public CustomClassLoader(URL[] classpath) {
-        super(Thread.currentThread().getContextClassLoader());
-        childClassLoader = new ChildClassLoader(classpath, new DetectClass(this.getParent()));
-    }
-
-    @Override
-    protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        try {
-            return childClassLoader.findClass(name);
-        } catch (ClassNotFoundException e) {
-            return super.loadClass(name, resolve);
-        }
-    }
-
-    private static class ChildClassLoader extends URLClassLoader {
-
-        private DetectClass realParent;
-
-        public ChildClassLoader(URL[] urls, DetectClass realParent) {
-            super(urls, null);
-            this.realParent = realParent;
-        }
-
-        @Override
-        public Class<?> findClass(String name) throws ClassNotFoundException {
-            try {
-                Class<?> loaded = super.findLoadedClass(name);
-                if (loaded != null) {
-                    return loaded;
-                }
-                return super.findClass(name);
-            } catch (ClassNotFoundException e) {
-                return realParent.loadClass(name);
+            try (FileOutputStream str = new FileOutputStream(pdfFile)) {
+                str.write(outStream.toByteArray());
             }
-        }
-    }
-
-    private static class DetectClass extends ClassLoader {
-
-        public DetectClass(ClassLoader parent) {
-            super(parent);
-        }
-
-        @Override
-        public Class<?> findClass(String name) throws ClassNotFoundException {
-            return super.findClass(name);
+        } catch (IOException | URISyntaxException | TransformerException | ConfigurationException | SAXException ex) {
+            throw new IOException("Error printing to PDF", ex);
         }
     }
 }
