8303cd5fe6a7a550279233bb1c8334f3f690c5ba
berry120 <berry120@gmail.com>
2018-07-19 16:51:34 +0100

    Update bundleJRE, drop 32 bit build support

--- Quelea/build-install.bat
+++ Quelea/build-install.bat
@@ -1,6 +1,6 @@
 set isxpath="c:\program files (x86)\inno setup 5"
 set isx=%isxpath%\iscc.exe
-set iwz=quelea.iss
-%isx% "%iwz%" /dMyAppVersion=%1 /O /F /q"Quelea"
+REM set iwz=quelea.iss
+REM %isx% "%iwz%" /dMyAppVersion=%1 /O /F /q"Quelea"
 set iwz=quelea64.iss
 %isx% "%iwz%" /dMyAppVersion=%1 /O /F /q"Quelea"
