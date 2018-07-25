701b22148572a848e333ee12e5d4882ab2927417
berry120 <berry120@gmail.com>
2018-07-25 10:44:55 +0100

    Snap fix

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $SNAP/jar
-desktop-launch java -DVLCJ_INITX=no -Xms1200m -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Djavafx.embed.singleThread=true -Dprism.dirtyopts=false -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
+desktop-launch $SNAP/jar/linjre64/bin/java -DVLCJ_INITX=no -Xms1200m -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Djavafx.embed.singleThread=true -Dprism.dirtyopts=false -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
