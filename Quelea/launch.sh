76547be799ff784a7a3e446e54e29ce3761fc759
berry120 <berry120@gmail.com>
2019-01-04 15:54:39 +0000

    Snap experiment

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd $SNAP/jar
-desktop-launch java -Djavafx.embed.singleThread=true -DVLCJ_INITX=no -Xms1200m -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Dprism.dirtyopts=false -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
+#desktop-launch java -Djavafx.embed.singleThread=true -DVLCJ_INITX=no -Xms1200m -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Dprism.dirtyopts=false -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
+desktop-launch java -DVLCJ_INITX=no -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Dprism.dirtyopts=false -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
