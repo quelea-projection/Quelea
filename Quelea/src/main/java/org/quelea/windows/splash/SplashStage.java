9fe9d307915fc90e26e38f428e4bb8ece4a87f44
berry120 <berry120@gmail.com>
2019-02-01 22:11:00 +0000

    Change splash screen type to be referenced in code rather than automatically from Travis.

--- Quelea/src/main/java/org/quelea/windows/splash/SplashStage.java
+++ Quelea/src/main/java/org/quelea/windows/splash/SplashStage.java
@@ -54,12 +54,7 @@ public class SplashStage extends Stage {
         initStyle(StageStyle.UNDECORATED);
         Utils.addIconsToStage(this);
         setTitle("Quelea " + LabelGrabber.INSTANCE.getLabel("loading.text") + "...");
-        Image splashImage;
-        if (QueleaProperties.VERSION.getUnstableName().toLowerCase().startsWith("beta")) {
-            splashImage = new Image(new File("icons/splash-beta.png").getAbsoluteFile().toURI().toString());
-        } else {
-            splashImage = new Image(new File("icons/splash-bare.png").getAbsoluteFile().toURI().toString());
-        }
+        Image splashImage = new Image(new File(QueleaProperties.VERSION.getUnstableName().getSplashPath()).getAbsoluteFile().toURI().toString());
         ImageView imageView = new ImageView(splashImage);
         Text loadingText = new Text(LabelGrabber.INSTANCE.getLabel("loading.text") + "...");
         Font loadingFont = Font.loadFont("file:icons/OpenSans-Bold.ttf", 32);
