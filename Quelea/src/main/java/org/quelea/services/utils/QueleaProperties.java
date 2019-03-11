393948f45fa05a9d8d8d54ead57657104650a1f3
berry120 <berry120@gmail.com>
2019-03-11 12:00:24 +0000

    Release

--- Quelea/src/main/java/org/quelea/services/utils/QueleaProperties.java
+++ Quelea/src/main/java/org/quelea/services/utils/QueleaProperties.java
@@ -41,7 +41,7 @@ import org.quelea.services.notice.NoticeDrawer.NoticePosition;
  */
 public final class QueleaProperties extends Properties {
 
-    public static final Version VERSION = new Version("2019.0", VersionType.CI);
+    public static final Version VERSION = new Version("2019.0", VersionType.RELEASE);
     private static QueleaProperties INSTANCE;
     private String userHome;
 
