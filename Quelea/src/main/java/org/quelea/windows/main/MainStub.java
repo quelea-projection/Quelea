bb57649bfb827e56e34791060dc9081599ebf642
Michael Berry <berry120@gmail.com>
2021-09-20 08:52:52 +0100

    Add main stub comment

--- Quelea/src/main/java/org/quelea/windows/main/MainStub.java
+++ Quelea/src/main/java/org/quelea/windows/main/MainStub.java
@@ -1,5 +1,12 @@
 package org.quelea.windows.main;
 
+/**
+ * For various reasons, with Java/OpenJFX 11+, we
+ * can't use a main class that extends the JavaFX
+ * Application class. This stub is therefore in
+ * place simply to act as an execution point, to
+ * then delegate to the real JavaFX main class.
+ */
 public class MainStub {
 
     public static void main(String[] args) {
