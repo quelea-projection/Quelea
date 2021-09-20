e316faac0f01f8506a532608e69d7efb7395ea8e
Michael Berry <berry120@gmail.com>
2021-09-20 09:11:10 +0100

    Java 11 / JFX 17 (#385)
    * Delete macjre.zip
    
    * Delete winjre64.zip
    
    * Remove lfs
    
    * Update bundles
    
    * J11 init
    
    * Fix travis
    
    * libAVPlayer should be stored natively
    
    * Add native web deps
    
    * Update changelog-2022.0.txt
    
    * Deprecation cleanup
    
    * Remove old 32 bit windows parts
    
    * Add MainStub
    
    * Remove jcenter
    
    * Add main stub comment

---
+++ Quelea/src/main/java/org/quelea/windows/main/MainStub.java
@@ -0,0 +1,15 @@
+package org.quelea.windows.main;
+
+/**
+ * For various reasons, with Java/OpenJFX 11+, we
+ * can't use a main class that extends the JavaFX
+ * Application class. This stub is therefore in
+ * place simply to act as an execution point, to
+ * then delegate to the real JavaFX main class.
+ */
+public class MainStub {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
