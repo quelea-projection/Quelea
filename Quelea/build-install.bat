6fe89c7103cfefcceac4ba10083ff38408b09f28
Michael Berry <berry120@gmail.com>
2021-09-20 08:48:07 +0100

    Remove old 32 bit windows parts

--- Quelea/build-install.bat
+++ Quelea/build-install.bat
@@ -1,6 +1,4 @@
 set isxpath="c:\program files (x86)\inno setup 5"
 set isx=%isxpath%\iscc.exe
-REM set iwz=quelea.iss
-REM %isx% "%iwz%" /dMyAppVersion=%1 /O /F /q"Quelea"
 set iwz=quelea64.iss
 %isx% "%iwz%" /dMyAppVersion=%1 /O /F /q"Quelea"
