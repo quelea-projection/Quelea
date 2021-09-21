9e35bbfb37d13518c36fd25da21c2f2e36b0aa07
Michael Berry <berry120@gmail.com>
2021-09-21 09:00:32 +0100

    Add debug

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -4,3 +4,4 @@ cd $SNAP/jar
 which java
 java -version
 java --add-exports=javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.runtime=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control=ALL-UNNAMED -Djdk.gtk.verbose=true -Djdk.gtk.version=2 -DVLCJ_INITX=no -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Dprism.dirtyopts=false -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
+#Trigger
