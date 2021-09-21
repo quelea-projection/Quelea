4ff20371d46c0bce7bc6cf586fbe60136659f6c4
Michael Berry <berry120@gmail.com>
2021-09-21 23:34:52 +0100

    Remove deprecated phonehome code

--- Quelea/src/main/java/org/quelea/services/utils/LoggerUtils.java
+++ Quelea/src/main/java/org/quelea/services/utils/LoggerUtils.java
@@ -17,18 +17,12 @@
  */
 package org.quelea.services.utils;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -103,30 +97,6 @@ public final class LoggerUtils {
                     synchronized(this) {
                         writeToLog(new SimpleFormatter().format(record));
                     }
-                    if(record.getLevel().intValue() >= Level.WARNING.intValue()) {
-                        StringBuilder sendText = new StringBuilder();
-                        sendText.append(record.getLevel().getName()).append("\n");
-                        sendText.append(new SimpleFormatter().format(record)).append("\n");
-                        if(record.getThrown() != null) {
-                            for(StackTraceElement e : record.getThrown().getStackTrace()) {
-                                sendText.append(e.toString()).append("\n");
-                            }
-                        }
-                        String errorText = null;
-                        try {
-                            errorText = URLEncoder.encode(sendText.toString(), "UTF-8");
-                        }
-                        catch(UnsupportedEncodingException ex) {
-                            //Nothing much here
-                        }
-                        final String fiErrorText = errorText;
-                        new Thread() {
-                            @Override
-                            public void run() {
-                                phoneHomeError(fiErrorText);
-                            }
-                        }.start();
-                    }
                 }
 
                 @Override
@@ -154,34 +124,4 @@ public final class LoggerUtils {
         }
     }
 
-    private static void phoneHomeError(String error) {
-        if(!QueleaProperties.get().getPhoneHome()) {
-            return;
-        }
-
-        String os = System.getProperty("os.name") + " : " + System.getProperty("os.version");
-
-        final StringBuilder urlStrBuilder = new StringBuilder("http://quelea.org/errorreport/store.php?os=");
-        urlStrBuilder.append(os);
-        urlStrBuilder.append("&version=");
-        urlStrBuilder.append(QueleaProperties.VERSION.getVersionString());
-        urlStrBuilder.append("&language=");
-        urlStrBuilder.append(Locale.getDefault().getDisplayLanguage(Locale.ENGLISH));
-
-        String urlStr = urlStrBuilder.toString().replace(" ", "%20");
-        urlStr += "&error=";
-        urlStr += error;
-
-        final StringBuilder result = new StringBuilder();
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(urlStr).openStream()));) {
-            String line;
-            while((line = reader.readLine()) != null) {
-                result.append(line).append('\n');
-            }
-        }
-        catch(IOException ex) {
-            //Oh well.
-        }
-
-    }
 }
