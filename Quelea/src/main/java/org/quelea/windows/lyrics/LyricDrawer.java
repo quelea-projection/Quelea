8db9c34a2c4e99043cd1f3f56a39eddd6ecda21e
Mark <79023646+a-developer-named-mark@users.noreply.github.com>
2021-10-03 17:21:27 +0100

    Issue 444 Use equalsIgnoreCase() instead of toLowerCase().equals() (#450)

--- Quelea/src/main/java/org/quelea/windows/lyrics/LyricDrawer.java
+++ Quelea/src/main/java/org/quelea/windows/lyrics/LyricDrawer.java
@@ -198,28 +198,28 @@ public class LyricDrawer extends WordDrawer {
         smallTextGroup.setEffect(smallshadow);
 
         if (curDisplayable instanceof BiblePassage) {
-            if (QueleaProperties.get().getSmallBibleTextPositionV().toLowerCase().equals("top")) {
-                if (QueleaProperties.get().getSmallBibleTextPositionH().toLowerCase().equals("left")) {
+            if (QueleaProperties.get().getSmallBibleTextPositionV().equalsIgnoreCase("top")) {
+                if (QueleaProperties.get().getSmallBibleTextPositionH().equalsIgnoreCase("left")) {
                     StackPane.setAlignment(smallTextGroup, Pos.TOP_LEFT);
                 } else {
                     StackPane.setAlignment(smallTextGroup, Pos.TOP_RIGHT);
                 }
             } else {
-                if (QueleaProperties.get().getSmallBibleTextPositionH().toLowerCase().equals("left")) {
+                if (QueleaProperties.get().getSmallBibleTextPositionH().equalsIgnoreCase("left")) {
                     StackPane.setAlignment(smallTextGroup, Pos.BOTTOM_LEFT);
                 } else {
                     StackPane.setAlignment(smallTextGroup, Pos.BOTTOM_RIGHT);
                 }
             }
         } else {
-            if (QueleaProperties.get().getSmallSongTextPositionV().toLowerCase().equals("top")) {
-                if (QueleaProperties.get().getSmallSongTextPositionH().toLowerCase().equals("left")) {
+            if (QueleaProperties.get().getSmallSongTextPositionV().equalsIgnoreCase("top")) {
+                if (QueleaProperties.get().getSmallSongTextPositionH().equalsIgnoreCase("left")) {
                     StackPane.setAlignment(smallTextGroup, Pos.TOP_LEFT);
                 } else {
                     StackPane.setAlignment(smallTextGroup, Pos.TOP_RIGHT);
                 }
             } else {
-                if (QueleaProperties.get().getSmallSongTextPositionH().toLowerCase().equals("left")) {
+                if (QueleaProperties.get().getSmallSongTextPositionH().equalsIgnoreCase("left")) {
                     StackPane.setAlignment(smallTextGroup, Pos.BOTTOM_LEFT);
                 } else {
                     StackPane.setAlignment(smallTextGroup, Pos.BOTTOM_RIGHT);
