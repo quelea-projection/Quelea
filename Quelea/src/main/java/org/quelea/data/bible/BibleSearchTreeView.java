a00c0d22290090d00df3afb9896d1aba881281d9
prakhar-198 <80689838+prakhar-198@users.noreply.github.com>
2021-10-12 13:49:41 +0530

    Update BibleSearchTreeView.java

--- Quelea/src/main/java/org/quelea/data/bible/BibleSearchTreeView.java
+++ Quelea/src/main/java/org/quelea/data/bible/BibleSearchTreeView.java
@@ -1,22 +1,34 @@
-/*
+/* 
  * This file is part of Quelea, free projection software for churches.
- *
- *
+ * 
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.quelea.data.bible;
 
+import java.util.Collection;
+import javafx.event.Event;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 /**
  * The TreeView responsible for showing search results in a TreeView model
