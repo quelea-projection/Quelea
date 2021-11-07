8b340a87872628933edff2d42c863a3102526472
Michael Berry <berry120@gmail.com>
2021-11-07 19:45:54 +0000

    Should work with latest version of worship him  - #273

--- Quelea/src/main/java/org/quelea/services/importexport/WorshipHimParser.java
+++ Quelea/src/main/java/org/quelea/services/importexport/WorshipHimParser.java
@@ -1,17 +1,17 @@
-/* 
+/*
  * This file is part of Quelea, free projection software for churches.
- * 
- * 
+ *
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,16 +21,15 @@ import com.healthmarketscience.jackcess.Database;
 import com.healthmarketscience.jackcess.DatabaseBuilder;
 import com.healthmarketscience.jackcess.Row;
 import com.healthmarketscience.jackcess.Table;
+import org.quelea.data.displayable.SongDisplayable;
+import org.quelea.services.utils.LoggerUtils;
+import org.quelea.windows.main.StatusPanel;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.quelea.data.displayable.SongDisplayable;
-import org.quelea.services.utils.LoggerUtils;
-import org.quelea.windows.main.StatusPanel;
 
 /**
  * A parser for parsing MediaShout databases, exported to txt format.
@@ -52,27 +51,19 @@ public class WorshipHimParser implements SongParser {
         List<SongDisplayable> ret = new ArrayList<>();
         try {
             Database d = new DatabaseBuilder()
-                    .setCharset(Charset.forName("CP1252"))
                     .setReadOnly(true)
                     .setFile(location)
                     .open();
-            Table table = d.getTable("Song list");
+
+            Table table = d.getTable("Songs");
             for (Row row : table) {
-                String title = row.get("Song").toString();
-                if(title.trim().isEmpty()) continue; //Ignore empty titled songs
-                String author = row.get("Author name").toString();
-                StringBuilder lyricsBuilder = new StringBuilder();
-                addToLyrics(lyricsBuilder, "Verse", 1, row.get("Stanza 1"));
-                for (int i = 1; i <= 5; i++) {
-                    addToLyrics(lyricsBuilder, "Chorus", i, row.get("Refrain " + i));
-                }
-                for (int i = 2; i <= 13; i++) {
-                    addToLyrics(lyricsBuilder, "Verse", i, row.get("Stanza " + i));
-                }
-                for (int i = 1; i <= 5; i++) {
-                    addToLyrics(lyricsBuilder, "Bridge", i, row.get("Bridge " + i));
-                }
-                String lyrics = lyricsBuilder.toString().trim();
+                String title = row.get("Title").toString();
+                if (title.trim().isEmpty()) continue; //Ignore empty titled songs
+                String author = row.get("Author").toString();
+                String lyrics = row.get("Lyrics").toString()
+                        .replace("\u0001", "\n")
+                        .replaceAll("\n\n+", "\n\n")
+                        .trim();
 
                 SongDisplayable song = new SongDisplayable(title, author);
                 song.setLyrics(lyrics);
@@ -82,14 +73,6 @@ public class WorshipHimParser implements SongParser {
             LOGGER.log(Level.WARNING, "Error importing worship hymn library", ex);
         }
         return ret;
-    }
-
-    private void addToLyrics(StringBuilder lyrics, String sectionTitle, int idx, Object section) {
-        if (section != null && !section.toString().trim().isEmpty()) {
-            lyrics.append(sectionTitle).append(" ").append(idx).append("\n");
-            lyrics.append(section.toString().replace("/", "").trim());
-            lyrics.append("\n\n");
-        }
     }
 
 }
