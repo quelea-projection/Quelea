c809ab1a16cfc986cfa8960a69f596e05d5045e2
Michael Berry <berry120@gmail.com>
2022-05-13 19:13:17 +0100

    Debug

--- Quelea/src/main/java/org/quelea/utils/VLCDiscovery.java
+++ Quelea/src/main/java/org/quelea/utils/VLCDiscovery.java
@@ -52,10 +52,10 @@ public class VLCDiscovery {
             directoryNames.add("/usr/local/lib64");
             directoryNames.add("/usr/lib/x86_64-linux-gnu");
             directoryNames.add("/usr/lib/i386-linux-gnu");
-            directoryNames.add(new File("../usr/lib").getAbsolutePath());
-            directoryNames.add(new File("../usr/lib64").getAbsolutePath());
-            directoryNames.add(new File("../usr/lib/x86_64-linux-gnu").getAbsolutePath());
-            directoryNames.add(new File("../usr/lib/i386-linux-gnu").getAbsolutePath());
+//            directoryNames.add(new File("../usr/lib").getAbsolutePath());
+//            directoryNames.add(new File("../usr/lib64").getAbsolutePath());
+//            directoryNames.add(new File("../usr/lib/x86_64-linux-gnu").getAbsolutePath());
+//            directoryNames.add(new File("../usr/lib/i386-linux-gnu").getAbsolutePath());
         }
 
     }
