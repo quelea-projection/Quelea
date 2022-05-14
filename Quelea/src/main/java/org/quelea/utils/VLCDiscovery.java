e5b9cd2af91996d76993f737d4009f79df2a9469
Michael Berry <berry120@gmail.com>
2022-05-14 09:13:59 +0100

    Debug

--- Quelea/src/main/java/org/quelea/utils/VLCDiscovery.java
+++ Quelea/src/main/java/org/quelea/utils/VLCDiscovery.java
@@ -7,10 +7,15 @@ import uk.co.caprica.vlcj.discovery.mac.DefaultMacNativeDiscoveryStrategy;
 import uk.co.caprica.vlcj.discovery.windows.DefaultWindowsNativeDiscoveryStrategy;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Custom discovery for VLC args. Includes folder where libvlc is located on
@@ -48,16 +53,44 @@ public class VLCDiscovery {
         @Override
         public void onGetDirectoryNames(List<String> directoryNames) {
             LOGGER.info("Adding search dirs with root dir " + new File(".").getAbsolutePath());
-            directoryNames.add("/usr/lib");
-            directoryNames.add("/usr/lib64");
-            directoryNames.add("/usr/local/lib");
-            directoryNames.add("/usr/local/lib64");
-            directoryNames.add("/usr/lib/x86_64-linux-gnu");
-            directoryNames.add("/usr/lib/i386-linux-gnu");
-            directoryNames.add(Paths.get("../usr/lib").toAbsolutePath().normalize().toString());
-            directoryNames.add(Paths.get("../usr/lib64").toAbsolutePath().normalize().toString());
-            directoryNames.add(Paths.get("../usr/lib/x86_64-linux-gnu").toAbsolutePath().normalize().toString());
-            directoryNames.add(Paths.get("../usr/lib/i386-linux-gnu").toAbsolutePath().normalize().toString());
+
+            Stream.of("/usr/lib", "/usr/lib64", "/usr/local/lib", "/usr/local/lib64", "/usr/lib/x86_64-linux-gnu", "/usr/lib/i386-linux-gnu")
+                    .filter(this::candidate)
+                    .forEach(directoryNames::add);
+
+            LOGGER.info("Found candidate dirs: " + directoryNames);
+
+//            directoryNames.add("/usr/lib");
+//            directoryNames.add("/usr/lib64");
+//            directoryNames.add("/usr/local/lib");
+//            directoryNames.add("/usr/local/lib64");
+//            directoryNames.add("/usr/lib/x86_64-linux-gnu");
+//            directoryNames.add("/usr/lib/i386-linux-gnu");
+//            directoryNames.add(Paths.get("../usr/lib").toAbsolutePath().normalize().toString());
+//            directoryNames.add(Paths.get("../usr/lib64").toAbsolutePath().normalize().toString());
+//            directoryNames.add(Paths.get("../usr/lib/x86_64-linux-gnu").toAbsolutePath().normalize().toString());
+//            directoryNames.add(Paths.get("../usr/lib/i386-linux-gnu").toAbsolutePath().normalize().toString());
+        }
+
+        public boolean candidate(String path) {
+            try {
+                var list = Files.list(Paths.get(path))
+                        .map(p -> p.getFileName().toString())
+                        .collect(Collectors.toList());
+                boolean v = false;
+                boolean core = false;
+                for (String s : list) {
+                    if (s.startsWith("libvlc.")) {
+                        v = true;
+                    }
+                    if (s.startsWith("libvlccore.")) {
+                        core = true;
+                    }
+                }
+                return v && core;
+            } catch (IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
         }
 
     }
