4147cf3327a44f02e010a61b45b5f4d7cc756064
Suhaas Srinivas <suhaas-livcd@users.noreply.github.com>
2022-10-10 13:57:10 -0700

    Update Image group displayable
    added import collections for add all

--- Quelea/src/main/java/org/quelea/data/displayable/ImageGroupDisplayable.java
+++ Quelea/src/main/java/org/quelea/data/displayable/ImageGroupDisplayable.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
