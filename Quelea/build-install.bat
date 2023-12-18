fb6edba2307eac11fc8ed74fbe41c309665cec18
berry120 <berry120@gmail.com>
2023-12-18 02:16:54 +0000

    Fix IS path

--- Quelea/build-install.bat
+++ Quelea/build-install.bat
@@ -1,4 +1,4 @@
-set isxpath="c:\program files (x86)\inno setup 5"
+set isxpath="c:\program files (x86)\inno setup 6"
 set isx=%isxpath%\iscc.exe
 set iwz=quelea64.iss
 %isx% "%iwz%" /dMyAppVersion=%1 /O /F /q"Quelea"
--- Quelea/build-install.sh
+++ Quelea/build-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wine "$HOME/.wine/drive_c/Program Files (x86)/Inno Setup 5/iscc.exe" /dMyAppVersion=$1 quelea64.iss /O /F /q"Quelea"
+wine "$HOME/.wine/drive_c/Program Files (x86)/Inno Setup 6/iscc.exe" /dMyAppVersion=$1 quelea64.iss /O /F /q"Quelea"
