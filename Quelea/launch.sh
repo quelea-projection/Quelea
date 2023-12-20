af9efcd13a036bd192d0cfdb607d3a4e808827b3
berry120 <berry120@gmail.com>
2023-12-20 00:14:00 +0000

    Set scanner path

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd $SNAP/jar
 export GST_PLUGIN_PATH=$SNAP/usr/lib/x86_64-linux-gnu/gstreamer-1.0
+export GST_PLUGIN_SCANNER=$SNAP/usr/lib/x86_64-linux-gnu/gstreamer-1.0/gst-plugin-scanner
 java --add-exports=javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.runtime=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control=ALL-UNNAMED -Djdk.gtk.verbose=true -Djdk.gtk.version=2 -DVLCJ_INITX=no -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Dprism.dirtyopts=false -Djavafx.cachedir=$SNAP_USER_COMMON -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
