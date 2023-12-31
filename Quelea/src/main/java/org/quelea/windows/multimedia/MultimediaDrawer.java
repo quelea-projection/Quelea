a43c3d761a388942a9a1a5b0328557d6d9963f7b
berry120 <berry120@gmail.com>
2023-12-31 15:47:09 +0000

    Fix #631

--- Quelea/src/main/java/org/quelea/windows/multimedia/MultimediaDrawer.java
+++ Quelea/src/main/java/org/quelea/windows/multimedia/MultimediaDrawer.java
@@ -10,6 +10,8 @@ import org.quelea.windows.main.DisplayableDrawer;
 import org.quelea.windows.video.VidDisplay;
 
 import java.io.File;
+import java.net.URI;
+import java.util.Objects;
 
 /**
  * @author tomaszpio@gmail.com, Michael
@@ -26,6 +28,9 @@ public class MultimediaDrawer extends DisplayableDrawer {
 
     @Override
     public void draw(Displayable displayable) {
+        MultimediaDisplayable multimediaDisplayable = (MultimediaDisplayable) displayable;
+        URI uri = new File(multimediaDisplayable.getLocation()).toURI();
+
         ImageView imageView = getCanvas().getNewImageView();
         imageView.imageProperty().bind(vidDisplay.imageProperty());
         imageView.setPreserveRatio(true);
@@ -35,19 +40,21 @@ public class MultimediaDrawer extends DisplayableDrawer {
         background.setImage(Utils.getImageFromColour(QueleaProperties.get().getStageBackgroundColor()));
         getCanvas().getChildren().add(0, background);
 
-        MultimediaDisplayable multimediaDisplayable = (MultimediaDisplayable) displayable;
-        controlPanel.reset();
-        vidDisplay.setURI(new File(multimediaDisplayable.getLocation()).toURI());
-        controlPanel.loadMultimedia(((MultimediaDisplayable) displayable).getLocation());
-        controlPanel.setOnPlay(vidDisplay::play);
-        controlPanel.setOnPause(vidDisplay::pause);
-        controlPanel.setOnStop(vidDisplay::stop);
-        controlPanel.setOnSeek(vidDisplay::seek);
-        controlPanel.setOnLoopChanged(vidDisplay::setLoop);
-        controlPanel.setOnVolumeChanged(vidDisplay::setVolume);
+        if(!Objects.equals(vidDisplay.getUri(), uri)) {
+            controlPanel.reset();
+            vidDisplay.setURI(uri);
+            controlPanel.loadMultimedia(((MultimediaDisplayable) displayable).getLocation());
+            controlPanel.setOnPlay(vidDisplay::play);
+            controlPanel.setOnPause(vidDisplay::pause);
+            controlPanel.setOnStop(vidDisplay::stop);
+            controlPanel.setOnSeek(vidDisplay::seek);
+            controlPanel.setOnLoopChanged(vidDisplay::setLoop);
+            controlPanel.setOnVolumeChanged(vidDisplay::setVolume);
 
-        vidDisplay.setOnPosChanged(controlPanel::setPosition);
-        vidDisplay.setOnFinished(controlPanel::reset);
+            vidDisplay.setOnPosChanged(controlPanel::setPosition);
+            vidDisplay.setOnFinished(controlPanel::reset);
+        }
+
     }
 
     public void setPlayVideo() {
