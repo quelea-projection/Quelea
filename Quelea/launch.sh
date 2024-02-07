a8cd8e8a128cf7113842dec707ab4c1c47db671b
berry120 <berry120@gmail.com>
2024-02-07 22:08:29 +0000

    Snapcraft debug

--- Quelea/launch.sh
+++ Quelea/launch.sh
@@ -3,5 +3,6 @@ cd $SNAP/jar
 export GST_PLUGIN_PATH=$SNAP/usr/lib/x86_64-linux-gnu/gstreamer-1.0
 export GST_PLUGIN_SCANNER=$SNAP/usr/lib/x86_64-linux-gnu/gstreamer-1.0/gst-plugin-scanner
 export G_FILENAME_ENCODING=UTF-8
-export XDG_DATA_DIRS=$SNAP/usr/share/ubuntu:$SNAP/usr/local/share/:$SNAP/usr/share/:$SNAP/var/lib/snapd/desktop
+export XDG_DATA_DIRS=/snap/quelea/current/usr/share/glib-2.0/schemas:$SNAP/usr/share/ubuntu:$SNAP/usr/local/share/:$SNAP/usr/share/:$SNAP/var/lib/snapd/desktop
+export XDG_DATA_DIR=/snap/quelea/current/usr/share/glib-2.0/schemas:$SNAP/usr/share/ubuntu:$SNAP/usr/local/share/:$SNAP/usr/share/:$SNAP/var/lib/snapd/desktop
 jvm/bin/java --add-exports=javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.runtime=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.event=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control=ALL-UNNAMED -Djdk.gtk.verbose=true -DVLCJ_INITX=no -Duser.dir=$SNAP/jar -Dfile.encoding=UTF-8 -Dprism.dirtyopts=false -Djavafx.cachedir=$SNAP_USER_COMMON -jar $SNAP/jar/Quelea.jar --userhome=$SNAP_USER_COMMON
